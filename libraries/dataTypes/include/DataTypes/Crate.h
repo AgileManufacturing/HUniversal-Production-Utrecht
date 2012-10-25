c3729397416a38e57f9950ebbe17329e50fdc44e
Daan Veltman <daanv1987@hotmail.com>
2012-10-25 10:17:33 +0200

    Changed QR detector to detect crates or reconfigure commands

--- libraries/dataTypes/include/DataTypes/Crate.h
+++ libraries/dataTypes/include/DataTypes/Crate.h
@@ -34,92 +34,93 @@
 #include <opencv2/core/core.hpp>
 #include <vector>
 
-class Crate {
-private:
-	//! Bounding rectangle saved after calling rect()
-	cv::RotatedRect bounds;
-	//! Fiducial points belonging to the crate
-	std::vector<cv::Point2f> points;
-	
-public:
-	//! The crate identifier
-	std::string name;
+namespace DataTypes {
+	class Crate {
+	private:
+		//! Bounding rectangle saved after calling rect()
+		cv::RotatedRect bounds;
+		//! Fiducial points belonging to the crate
+		std::vector<cv::Point2f> points;
+		
+	public:
+		//! The crate identifier
+		std::string name;
 
-	/*! \brief The Crate constructor
-	 *
-	 *  Constructs a crate without any fiducial points
-	 */
-	Crate();
+		/*! \brief The Crate constructor
+		 *
+		 *  Constructs a crate without any fiducial points
+		 */
+		Crate();
 
-	/*! \brief The Crate constructor
-	 *
-	 *  Constructs a crate with the given three fiducial points.
-	 *  The fiducial points ordering must be left-handed.
-	 */
-	Crate(const std::vector<cv::Point2f>& points);
+		/*! \brief The Crate constructor
+		 *
+		 *  Constructs a crate with the given three fiducial points.
+		 *  The fiducial points ordering must be left-handed.
+		 */
+		Crate(const std::vector<cv::Point2f>& points);
 
-	/*! \brief The Crate constructor
-	 *
-	 *  Constructs a crate with the given three fiducial points
-	 *  and a name. The fiducial points ordering must be left-handed.
-	 */
-	Crate(std::string name, const std::vector<cv::Point2f>& points);
+		/*! \brief The Crate constructor
+		 *
+		 *  Constructs a crate with the given three fiducial points
+		 *  and a name. The fiducial points ordering must be left-handed.
+		 */
+		Crate(std::string name, const std::vector<cv::Point2f>& points);
 
-	/*! \brief The Crate copy-constructor
-	 *
-	 *  Constructs a crate with the same values as the
-	 *  given crate, fiducial points are deep-copied.
-	 */
-	Crate(const Crate& crate);
+		/*! \brief The Crate copy-constructor
+		 *
+		 *  Constructs a crate with the same values as the
+		 *  given crate, fiducial points are deep-copied.
+		 */
+		Crate(const Crate& crate);
 
-	//! THe Crate deconstructor
-	virtual ~Crate();
+		//! THe Crate deconstructor
+		virtual ~Crate();
 
-	/*! \brief Calculate distance
-	 *
-	 *  Calculates the distance between two fiducial points
-	 */
-	static inline float distance(const cv::Point2f& pt1, const cv::Point2f& pt2) {
-		float dx = pt1.x-pt2.x;
-		float dy = pt1.y-pt2.y;
-		return sqrt(dx*dx+dy*dy);
-	}
+		/*! \brief Calculate distance
+		 *
+		 *  Calculates the distance between two fiducial points
+		 */
+		static inline float distance(const cv::Point2f& pt1, const cv::Point2f& pt2) {
+			float dx = pt1.x-pt2.x;
+			float dy = pt1.y-pt2.y;
+			return sqrt(dx*dx+dy*dy);
+		}
 
-	/*! \brief Order a list of fiducial points
-	 *
-	 *  Orders a vector with 3 fiducial points according
-	 *  to the clockwise crate ordering.
-	 */
-	static void order(std::vector<cv::Point2f>& points);
+		/*! \brief Order a list of fiducial points
+		 *
+		 *  Orders a vector with 3 fiducial points according
+		 *  to the clockwise crate ordering.
+		 */
+		static void order(std::vector<cv::Point2f>& points);
 
-	/*! \brief Generate a rotated bounding rectangle
-	 *
-	 *  Generates a RotatedRect that represents the
-	 *  crate. This rectangle is cached for subsequent
-	 *  calls to rect().
-	 */
-	cv::RotatedRect rect();
+		/*! \brief Generate a rotated bounding rectangle
+		 *
+		 *  Generates a RotatedRect that represents the
+		 *  crate. This rectangle is cached for subsequent
+		 *  calls to rect().
+		 */
+		cv::RotatedRect rect();
 
-	/*! \brief Get the fiducial points
-	 *
-	 * Gets a copy of the fiducial points
-	 * that represent the crate.
-	 */
-	std::vector<cv::Point2f> getPoints() const;
+		/*! \brief Get the fiducial points
+		 *
+		 * Gets a copy of the fiducial points
+		 * that represent the crate.
+		 */
+		std::vector<cv::Point2f> getPoints() const;
 
-	/*! \brief Set the fiducial points
-	 *
-	 *  Sets the new fiducial points and resets the
-	 *  bounding rectangle.
-	 */
-	void setPoints(std::vector<cv::Point2f>& points);
+		/*! \brief Set the fiducial points
+		 *
+		 *  Sets the new fiducial points and resets the
+		 *  bounding rectangle.
+		 */
+		void setPoints(std::vector<cv::Point2f>& points);
 
-	/*! \brief Draw the rectangle in the image
-	 *
-	 *  Draws the rectangle in the image including the
-	 *  fiducial points, angle and bounding rectangle.
-	 */
-	void draw(cv::Mat& image);
-};
-
+		/*! \brief Draw the rectangle in the image
+		 *
+		 *  Draws the rectangle in the image including the
+		 *  fiducial points, angle and bounding rectangle.
+		 */
+		void draw(cv::Mat& image);
+	};
+}
 #endif /* CRATE_H_ */
