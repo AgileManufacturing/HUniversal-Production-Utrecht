b50fa6451ff60513799e05877289eabf766bec6c
Koen Braham <koenbraham@gmail.com>
2012-11-12 14:02:32 +0100

    Increased min Z value

--- libraries/deltaRobot/include/DeltaRobot/Measures.h
+++ libraries/deltaRobot/include/DeltaRobot/Measures.h
@@ -113,7 +113,7 @@ namespace DeltaRobot{
 		 * @var double BOUNDARY_BOX_MIN_Z
 		 * The min z-coordinate of the effector boundary box in millimeters.
 		 **/
-		const double BOUNDARY_BOX_MIN_Z = -250;
+		const double BOUNDARY_BOX_MIN_Z = -290;
 
 		/**
 		 * @var double BOUNDARY_BOX_MAX_Z
