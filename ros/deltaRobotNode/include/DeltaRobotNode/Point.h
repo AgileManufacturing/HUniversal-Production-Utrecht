ba4bea2a198e5fd35938ead3fc6fc94e1b5cedaf
pndewit <dodoiva@gmail.com>
2012-11-19 13:07:33 +0100

    Point.h added

---
+++ ros/deltaRobotNode/include/DeltaRobotNode/Point.h
@@ -0,0 +1,50 @@
+/**
+ * @file Point.h
+ * @brief Data class for point objects.
+ * @date Created: 2012-11-19
+ *
+ * @author Patrick de Wit
+ *
+ * @section LICENSE
+ * License: newBSD
+ * Copyright © 2012, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+#ifndef POINT_H
+#define POINT_H
+
+#include "ros/ros.h"
+
+namespace deltaRobotNodeNamespace {
+	class Point {
+	public:
+		int x, y, z, maxAcceleration;
+
+		Point(int xValue, int yValue, int zValue, int speed) {
+			x = xValue;
+			y = yValue;
+			z = zValue;
+			maxAcceleration = speed;
+		}
+		virtual ~Point();
+	};
+}
+
+#endif
