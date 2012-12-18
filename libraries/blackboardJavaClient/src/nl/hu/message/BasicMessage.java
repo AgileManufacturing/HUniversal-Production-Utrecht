34a931fde80b0613530ce257dac1eb1018b4deff
Dick van der Steen <agileman@agilemanufacturing-equiplet1-visioncore.(none)>
2012-12-18 11:35:39 +0100

    Added BasicMessage to java blackboard library

---
+++ libraries/blackboardJavaClient/src/nl/hu/message/BasicMessage.java
@@ -0,0 +1,17 @@
+package nl.hu.message;
+
+public class BasicMessage
+{
+	public String topic;
+
+	public Object message;
+
+	public BasicMessage(String topic, Object message)
+	{
+		this.topic = topic;
+		this.message = message;
+	}
+
+
+
+}
