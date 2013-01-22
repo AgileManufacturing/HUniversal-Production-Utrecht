8b399e0e0b2b7ce4e03f42d401d7066ec2b16b4d
Dick <dick@Dick-Laptop.(none)>
2013-01-22 11:03:47 +0100

    initial restructure

--- cmake/FindScons.cmake
+++
@@ -1,9 +0,0 @@
-include(FindPackageHandleStandardArgs)
-
-FIND_PROGRAM(SCONS_EXECUTABLE
-  NAMES "scons"
-  PATH /usr/bin
-  DOC "SCons - Build system"
-)
-
-find_package_handle_standard_args(SCONS DEFAULT_MSG SCONS_EXECUTABLE)
--- libraries/blackboardJavaClient/src/nl/hu/client/ISubscriber.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.client;
-
-import java.util.Map;
-
-public interface ISubscriber{
-	public void onMessage(String topic);
-}
