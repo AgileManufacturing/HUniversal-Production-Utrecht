9d6eed1f4b2eaa4414f7cc8f72c476efbaec5bfa
apvanzanten <ap.vanzanten@gmail.com>
2013-03-13 16:18:05 +0100

    Added a dummy module. Compiles, runs, but for some reason the services do not work. Error on trying to call /RequestStateChange service:  ERROR: Unable to load type [rexos_mast/StateChange].  Have you typed 'make' in [rexos_mast]?

---
+++ src/ros/dummy_module_node/include/dummy_module_node/DummyModuleNode.h
@@ -0,0 +1,66 @@
+/**
+ * @file DummyModuleNode.h
+ * @brief A dummy module!
+ * @date Created: 2013-03-13
+ *
+ * @author Arjen van Zanten
+ * @author Ammar Abdulamir
+ *
+ * @section LICENSE
+ * License: newBSD
+ * Copyright © 2013, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+#ifndef DUMMYMODULENODE_H
+#define DUMMYMODULENODE_H
+
+#include "ros/ros.h"
+#include "rexos_std_srvs/Module.h"
+
+#include <rexos_utilities/Utilities.h>
+#include <rexos_mast/StateMachine.h>
+
+// GCC system header to suppress libjson warnings
+#pragma GCC system_header
+#include <Libjson/libjson.h>
+// ---------------------------------------------
+
+class DummyModuleNode : public rexos_mast::StateMachine{
+public:
+	DummyModuleNode(int equipletID, int moduleID);
+	virtual ~DummyModuleNode();
+	
+	int transitionSetup();
+	int transitionShutdown();
+	int transitionStart();
+	int transitionStop();
+
+	// services
+	bool outputCoords(rexos_std_srvs::Module::Request &req, rexos_std_srvs::Module::Response &res);
+
+private:
+	/**
+	 * @var ros::ServiceServer outputCoords
+	 * Service for outputting coordinates
+	 **/
+	ros::ServiceServer outputCoordsService;
+};
+
+#endif
---
+++ src/ros/dummy_module_node/src/DummyModuleNode.cpp
@@ -0,0 +1,102 @@
+/**
+ * @file DummyModuleNode.cpp
+ * @brief A dummy module!
+ * @date Created: 2013-03-13
+ *
+ * @author Arjen van Zanten
+ * @author Ammar Abdulamir
+ *
+ * @section LICENSE
+ * License: newBSD
+ * Copyright © 2013, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+ #include "dummy_module_node/DummyModuleNode.h"
+ #include "dummy_module_node/Services.h"
+
+ // @cond HIDE_NODE_NAME_FROM_DOXYGEN
+#define NODE_NAME "DummyModuleNode"
+// @endcond
+
+DummyModuleNode::DummyModuleNode(int equipletID, int moduleID) : rexos_mast::StateMachine(equipletID, moduleID) {
+	std::cout << "Constructing DummyModuleNode. EquipletID: " << equipletID << "ModuleID: " << moduleID << std::endl;
+
+	ros::NodeHandle nodeHandle;
+
+	outputCoordsService = nodeHandle.advertiseService(DummyModuleNodeServices::OUTPUTCOORDS, &DummyModuleNode::outputCoords, this);
+}
+
+bool DummyModuleNode::outputCoords(rexos_std_srvs::Module::Request &req, rexos_std_srvs::Module::Response &res){
+	ROS_INFO("outputCoords called");
+	std::cout << std::string(req.json) << std::endl;
+	return true;
+}
+
+int DummyModuleNode::transitionSetup(){
+	ROS_INFO("Setup transition called");
+	setState(rexos_mast::setup);
+
+	return 0;
+}
+
+int DummyModuleNode::transitionShutdown(){
+	ROS_INFO("Shutdown transition called");
+	setState(rexos_mast::shutdown);
+
+	return 0;
+}
+
+int DummyModuleNode::transitionStart(){
+	ROS_INFO("Start transition called");
+	setState(rexos_mast::start);
+
+	return 0;
+}
+
+int DummyModuleNode::transitionStop(){
+	ROS_INFO("Stop transition called");
+	setState(rexos_mast::stop);
+	
+	return 0;
+}
+
+DummyModuleNode::~DummyModuleNode(){
+	ROS_INFO("Destructing");
+}
+
+int main(int argc, char **argv){
+	int equipletID = 0;
+	int moduleID = 0;
+
+	if(argc < 3 || !(rexos_utilities::stringToInt(equipletID, argv[1]) == 0 && rexos_utilities::stringToInt(moduleID, argv[2]) == 0)){ 	 	
+		ROS_INFO("Cannot read equiplet id and/or moduleId from commandline please use correct values.");
+		return -1;
+	}
+
+	ros::init(argc, argv, NODE_NAME);
+
+	ROS_INFO("Creating Dummy Module");
+
+	DummyModuleNode dummy(equipletID, moduleID);
+
+	ROS_INFO("Running StateEngine");
+	ros::spin();
+	return 0;
+}
