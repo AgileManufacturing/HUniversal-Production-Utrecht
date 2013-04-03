bf84fe81947b77efabc5a0125a13c30308af0b3d
Hessel Meulenbeld <hesselmeulenbeld@gmail.com>
2013-04-03 10:27:46 +0200

    Initial productstepmessage

---
+++ src/mas/equipletAgent/src/nl/hu/ProductStepMessage.java
@@ -0,0 +1,58 @@
+/**
+ * @file ProductStepMessage.java
+ * @brief Provides a message for the productstep blackboard
+ * @date Created: 2013-04-03
+ *
+ * @author Hessel Meulenbeld
+ *
+ * @section LICENSE
+ * License: newBSD
+ *
+ * Copyright © 2012, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+package nl.hu;
+
+/**
+ * Implementation of a message for the productstep blackboard
+ */
+public class ProductStepMessage{
+	public int product_step_id;
+	public int product_agent_id;
+	public String type;
+	public Dictionary parameters;
+	public Object input_parts;
+	public Object output_parts;
+	public int status;
+	public Object schedule_data;
+
+	public ProductStepMessage(int product_step_id, int product_agent_id,
+							  String type, Dictionary parameters,
+							  Object input_parts, Object output_parts,
+							  int status, Object schedule_data){
+		this.product_step_id = product_step_id;
+		this.product_agent_id = product_agent_id;
+		this.type = type;
+		this.parameters = parameters;
+		this.input_parts = input_parts;
+		this.output_parts = output_parts;
+		this.status = status;
+		this.schedule_data = schedule_data
+	}
+}
