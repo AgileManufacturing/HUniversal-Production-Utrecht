c614ecb327083e58f40fccae408b80f2cd90dbdb
Asive <rickyvanrijn@gmail.com>
2013-04-04 11:51:59 +0200

    Edited scheduling/Scheduler.java

--- src/scheduling/Scheduler.java
+++ src/scheduling/Scheduler.java
@@ -7,13 +7,14 @@ public class Scheduler {
 	public Scheduler(Object[] equipletList, Object productionStep){
 		blackboard.connect(scheduleBlackboard);
 		
-		//TODO tijdSloten berekenen welke vrij zijn	en eerste vrije slot kiezen	
+		//TODO tijdsloten berekenen welke vrij zijn	en eerste vrije slot kiezen	
+		int startTimeSlot = 0;
 		
 		schedule(equipletList[0], startTimeSlot);
 		
 	}
 	
 	private void schedule(Object equiplet, int startTimeslot){
-		
+		blackboard.place(equipletID, duration);
 	}
 }
