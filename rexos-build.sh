84af8a47859c2355e02453051008b373a8d16a20
apvanzanten <ap.vanzanten@gmail.com>
2013-04-18 16:27:05 +0200

    Fixed up the java ant buildsystem. Now builds all current source. Removed blackboardReader since it will be replaced soonish. Added the ant build to rexos-build.sh.

--- rexos-build.sh
+++ rexos-build.sh
@@ -1,3 +1,8 @@
+echo -e "\033[36m===== Building C++ =====\033[0m"
 catkin_make
 source devel/setup.sh
 rospack list > /dev/null
+echo ""
+echo -e "\033[36m===== Building JAVA =====\033[0m"
+ant -buildfile src/java/build.xml
+source build/java/.export-classpath
