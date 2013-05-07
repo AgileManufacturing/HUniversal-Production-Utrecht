3893ef519444aa3aabf159e58ce9fad7907e7196
Wouter Veen <wouterbrveen@gmail.com>
2013-05-07 11:55:07 +0200

    Made chances

--- src/java/rexos/mas/hardware_agent/HardwareAgent.java
+++ src/java/rexos/mas/hardware_agent/HardwareAgent.java
@@ -32,7 +32,10 @@ package rexos.mas.hardware_agent;
  **/
 
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 
+import java.sql.ResultSet;
 import java.util.HashMap;
 
 import org.bson.types.ObjectId;
@@ -46,6 +49,10 @@ import rexos.libraries.blackboard_client.InvalidDBNamespaceException;
 import rexos.libraries.blackboard_client.MongoOperation;
 import rexos.libraries.blackboard_client.OplogEntry;
 import rexos.libraries.blackboard_client.FieldUpdateSubscription.MongoUpdateLogOperation;
+import rexos.libraries.knowledgedb_client.KnowledgeDBClient;
+import rexos.libraries.knowledgedb_client.KnowledgeException;
+import rexos.libraries.knowledgedb_client.Queries;
+import rexos.libraries.knowledgedb_client.Row;
 import rexos.mas.data.DbData;
 import rexos.mas.data.ScheduleData;
 import rexos.mas.equiplet_agent.ProductStepMessage;
@@ -63,20 +70,22 @@ public class HardwareAgent extends Agent implements BlackboardSubscriber {
 
 	private BlackboardClient serviceStepBBClient, equipletStepBBClient;
 	private DbData dbData;
-	private HashMap<Integer, Module> leadingModules;
-		
-	public void registerLeadingModule(int serviceId, Module module) {
-		leadingModules.put(serviceId, module);
+	private HashMap<Integer, Module> Modules;
+	private HashMap<Integer, Integer> leadingModuleForStep;
+	private HardwareAgent hardwareAgent;
+
+	public void registerModule(int serviceId, Module module) {
+		Modules.put(serviceId, module);
 	}
-	
-	public Module getLeadingModule(int serviceId) {
-		return leadingModules.get(serviceId);
+
+	public Module getModule(int serviceId) {
+		return Modules.get(serviceId);
 	}
-		
+
 	@Override
 	public void setup() {
 		System.out.println("Hardware agent " + this + " reporting.");
-		leadingModules = new HashMap<Integer, Module>();
+		Modules = new HashMap<Integer, Module>();
 
 		// TODO fill in host, database and collection
 		Object[] args = getArguments();
@@ -88,41 +97,69 @@ public class HardwareAgent extends Agent implements BlackboardSubscriber {
 			serviceStepBBClient = new BlackboardClient(dbData.getIp());
 			serviceStepBBClient.setDatabase(dbData.getName());
 			serviceStepBBClient.setCollection("ServiceStepsBlackBoard");
-					
-			FieldUpdateSubscription statusSubscription = new FieldUpdateSubscription("status", this);
+
+			FieldUpdateSubscription statusSubscription = new FieldUpdateSubscription(
+					"status", this);
 			statusSubscription.addOperation(MongoUpdateLogOperation.SET);
 			serviceStepBBClient.subscribe(statusSubscription);
-			
+
 			equipletStepBBClient = new BlackboardClient(dbData.getIp());
 			equipletStepBBClient.setDatabase(dbData.getName());
 			equipletStepBBClient.setCollection("EquipletStepsBlackBoard");
-			equipletStepBBClient.subscribe(new BasicOperationSubscription(MongoOperation.UPDATE, this));
+			equipletStepBBClient.subscribe(new BasicOperationSubscription(
+					MongoOperation.UPDATE, this));
 		} catch (Exception e) {
 			e.printStackTrace();
 			doDelete();
 		}
-		
+
 		EvaluateDuration evaluateDurationBehaviour = new EvaluateDuration(this);
 		addBehaviour(evaluateDurationBehaviour);
-		
+
 		FillPlaceholders fillPlaceholdersBehaviour = new FillPlaceholders(this);
 		addBehaviour(fillPlaceholdersBehaviour);
-		
+
 		CheckForModules checkForModules = new CheckForModules(this);
 		addBehaviour(checkForModules);
-		
-		///Register modules
-		
-		/// modulefactory aan het werk gezet
-		
-		
+
+		// /Register modules
+		KnowledgeDBClient client;
+		try {
+			client = KnowledgeDBClient.getClient();
+
+			Row[] resultSet;
+
+			resultSet = client.executeSelectQuery(Queries.MODULES);
+
+			for (int i = 0; i < resultSet.length; i++) {
+				System.out.println(resultSet[i]);
+
+				// neem id van module
+				// geef id aan modulefactory,
+				// je krijgt een module terug,
+				//
+				// register module bij de hardwareagent..
+
+			}
+			System.out.println();
+
+		} catch (KnowledgeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// kijk voor alle modules in de hashmap modules voor welke stap/stappen
+		// deze module leidend is
+		// en zet het id van de module, en het id van de bijbehorende step in de
+		// leadingModuleForStep Hashmap
+
 		// for now: use precompiled grippermodule class
-		GripperModule gp = new GripperModule();
-		registerLeadingModule(1, gp);
-		DeltaRobotModule drm = new DeltaRobotModule();
-		registerLeadingModule(2, drm);
-		///		
-		
+		// GripperModule gp = new GripperModule();
+		// registerLeadingModule(1, gp);
+		// DeltaRobotModule drm = new DeltaRobotModule();
+		// registerLeadingModule(2, drm);
+		// /
+
 	}
 
 	@Override
@@ -130,33 +167,35 @@ public class HardwareAgent extends Agent implements BlackboardSubscriber {
 		// TODO implement graceful death
 	}
 
-	public BlackboardClient getServiceStepsBBClient(){
+	public BlackboardClient getServiceStepsBBClient() {
 		return serviceStepBBClient;
 	}
-	
-	
+
 	@Override
 	public void onMessage(MongoOperation operation, OplogEntry entry) {
 		switch (entry.getNamespace().split("\\.")[1]) {
 		case "ServiceStepsBlackboard":
 			switch (operation) {
 			case UPDATE:
-						
+
 				ObjectId id = entry.getTargetObjectId();
 				try {
-					ServiceStepMessage serviceStep = new ServiceStepMessage((BasicDBObject)serviceStepBBClient.findDocumentById(id));
-					
-					Module leadingModule = getLeadingModule(serviceStep
-							.getServiceId());
+					ServiceStepMessage serviceStep = new ServiceStepMessage(
+							(BasicDBObject) serviceStepBBClient
+									.findDocumentById(id));
+
+					Module leadingModule = getModule(serviceStep.getServiceId());
 					EquipletStepMessage[] equipletSteps = leadingModule
-							.getEquipletSteps(serviceStep.getType(),serviceStep.getParameters());
-															
-					for(EquipletStepMessage eq : equipletSteps){
-					
-						equipletStepBBClient.insertDocument(eq.toBasicDBObject());					
-					
+							.getEquipletSteps(serviceStep.getType(),
+									serviceStep.getParameters());
+
+					for (EquipletStepMessage eq : equipletSteps) {
+
+						equipletStepBBClient.insertDocument(eq
+								.toBasicDBObject());
+
 					}
-					
+
 				} catch (InvalidDBNamespaceException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -164,27 +203,28 @@ public class HardwareAgent extends Agent implements BlackboardSubscriber {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 				break;
-				//$CASES-OMITTED$
+			// $CASES-OMITTED$
 			default:
 				break;
 			}
 			break;
 		case "EquipletStepsBlackboard":
-			switch(operation){
+			switch (operation) {
 			case UPDATE:
 				try {
 					ObjectId id = entry.getTargetObjectId();
 					BasicDBObject query = new BasicDBObject();
 					query.put("_id", id);
-					DBObject equipletStep = equipletStepBBClient.findDocuments(query).get(0);
+					DBObject equipletStep = equipletStepBBClient.findDocuments(
+							query).get(0);
 				} catch (InvalidDBNamespaceException | GeneralMongoException e) {
 					// TODO Error no document
 					e.printStackTrace();
 				}
 				break;
-				//$CASES-OMITTED$
+			// $CASES-OMITTED$
 			default:
 				break;
 			}
