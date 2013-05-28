4526e03df8f12f08ce6d011497b21465615dd8e1
jwillebrands <jwillebrands@gmail.com>
2013-05-28 15:04:50 +0200

    Fixed bug in query creation for multiple subscriptions. Due to a missing or it was trying to match all the subscriptions, rather than any one of them.

--- src/java/rexos/libraries/blackboard_client/BlackboardClient.java
+++ src/java/rexos/libraries/blackboard_client/BlackboardClient.java
@@ -472,8 +472,11 @@ public class BlackboardClient {
 			subs[i] = subscriptions.get(i).getQuery();
 		}
 		
-		DBObject query = QueryBuilder.start("ns").is(currentDatabase.getName() + "." + currentCollection.getName())
-				.and(subs).get();
+		DBObject query = 
+				// If namespace is current database and collection.
+				QueryBuilder.start("ns").is(currentDatabase.getName() + "." + currentCollection.getName())
+				// And it matches one of the subscriptions
+				.and(QueryBuilder.start().or(subs).get()).get();
 
 		try {
 			if (oplogUser != null) {
