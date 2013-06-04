77b683878228bde8dcb79794766726efdaa44aa3
jwillebrands <jwillebrands@gmail.com>
2013-06-04 14:38:22 +0200

    Created classes for the various subscription types.

---
+++ src/cpp/libraries/rexos_blackboard_cpp_client/include/rexos_blackboard_cpp_client/BlackboardSubscription.h
@@ -0,0 +1,56 @@
+/**
+ * @file BlackboardSubscription.h
+ * @brief 
+ * @date Created: 3 jun. 2013
+ *
+ * @author Jan-Willem Willebrands
+ *
+ * @section LICENSE
+ * License: newBSD
+ *
+ * Copyright © 2013, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+#ifndef BLACKBOARDSUBSCRIPTION_H_
+#define BLACKBOARDSUBSCRIPTION_H_
+
+#include <string>
+#include "mongo/client/dbclientinterface.h"
+
+namespace Blackboard {
+
+class OplogEntry;
+class BlackboardSubscriber;
+
+class BlackboardSubscription {
+public:
+	virtual bool getQuery(mongo::Query * query_out) const = 0;
+	virtual bool matchesWithEntry(const OplogEntry & oplogEntry) const = 0;
+	BlackboardSubscriber & getSubscriber() const;
+
+	BlackboardSubscription(BlackboardSubscriber & subscriber);
+	virtual ~BlackboardSubscription() {};
+
+private:
+	BlackboardSubscriber & subscriber;
+};
+
+} /* namespace Blackboard */
+#endif /* BLACKBOARDSUBSCRIPTION_H_ */
---
+++ src/cpp/libraries/rexos_blackboard_cpp_client/src/FieldUpdateSubscription.cpp
@@ -0,0 +1,104 @@
+/**
+ * @file FieldUpdateSubscription.cpp
+ * @brief 
+ * @date Created: 3 jun. 2013
+ *
+ * @author Jan-Willem Willebrands
+ *
+ * @section LICENSE
+ * License: newBSD
+ *
+ * Copyright © 2013, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+#include "rexos_blackboard_cpp_client/FieldUpdateSubscription.h"
+#include "rexos_blackboard_cpp_client/OplogEntry.h"
+
+namespace Blackboard {
+
+FieldUpdateSubscription::FieldUpdateSubscription(std::string fieldName, BlackboardSubscriber & subscriber) :
+		BasicOperationSubscription(MongoOperation::UPDATE, subscriber),
+		fieldName(fieldName)
+{
+}
+
+void FieldUpdateSubscription::addOperation(MongoUpdateLogOperation operation) {
+	if (std::find(subscribedOperations.begin(), subscribedOperations.end(), operation) == subscribedOperations.end()) {
+		subscribedOperations.push_back(operation);
+	}
+}
+
+void FieldUpdateSubscription::removeOperation(MongoUpdateLogOperation operation) {
+	std::vector<MongoUpdateLogOperation>::iterator iter =
+			std::find(subscribedOperations.begin(), subscribedOperations.end(), operation);
+
+	if (iter != subscribedOperations.end()) {
+		subscribedOperations.erase(iter);
+	}
+}
+
+bool FieldUpdateSubscription::getQuery(mongo::Query * query_out) const {
+	if (!subscribedOperations.size()) {
+		return false;
+	}
+
+	mongo::BSONObjBuilder builder;
+	mongo::BSONArrayBuilder orArray;
+
+	mongo::Query query;
+	BasicOperationSubscription::getQuery(&query);
+
+	builder.appendElements(query.getFilter());
+
+	for (std::vector<MongoUpdateLogOperation>::const_iterator iter = subscribedOperations.begin(); iter != subscribedOperations.end() ; iter++) {
+		std::stringstream fieldString;
+		fieldString << OplogEntry::UPDATE_DOC_FIELD << '.';
+
+		std::string oper = stringForUpdateOperation(*iter);
+		if (!oper.empty()) {
+			fieldString << oper << '.';
+		}
+
+		fieldString << fieldName;
+
+		orArray.append(BSON(fieldString.str() << BSON("$exists" << true)));
+	}
+
+	builder.append("$or", orArray.arr());
+	*query_out = mongo::Query(builder.obj());
+	return true;
+}
+
+bool FieldUpdateSubscription::matchesWithEntry(const OplogEntry& entry) const {
+	return true;
+}
+
+std::string FieldUpdateSubscription::stringForUpdateOperation(MongoUpdateLogOperation operation) const {
+	switch(operation) {
+	case MongoUpdateLogOperation::SET:
+		return "$set";
+	case MongoUpdateLogOperation::UNSET:
+		return "$unset";
+	default:
+		return "";
+	}
+}
+
+} /* namespace Blackboard */
