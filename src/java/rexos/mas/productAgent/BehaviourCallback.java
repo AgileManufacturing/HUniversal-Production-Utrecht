66e444d7dbac266798af173a64d4d71d9ff43bc1
Mike Schaap <mikeschaap@gmail.com>
2013-06-24 09:52:32 +0200

    added new classes for the overview architecture

---
+++ src/java/rexos/mas/productAgent/BehaviourCallback.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * Project: Product Agent
+ *
+ * Package: rexos.mas.productAgent
+ *
+ * File: BehaviourCallback.java
+ *
+ * Author: Mike Schaap
+ *
+ * Version: 1.0
+ *
+ */
+package rexos.mas.productAgent;
+
+import rexos.mas.data.BehaviourStatus;
+
+/**
+ * @author Mike
+ *
+ */
+public interface BehaviourCallback{
+	
+	
+	public void handleCallback(BehaviourStatus bs);
+	
+}
