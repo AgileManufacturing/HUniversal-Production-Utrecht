166add525a113dd9fbdfda0593c21da3952de8ad
Hessel Meulenbeld <hesselmeulenbeld@gmail.com>
2013-06-28 17:09:12 +0200

    bug fix

--- src/java/rexos/mas/productAgent/SchedulerBehaviour.java
+++ src/java/rexos/mas/productAgent/SchedulerBehaviour.java
@@ -220,7 +220,7 @@ public class SchedulerBehaviour extends Behaviour {
 					}
 				}
 			} else {
-				freetimeslots.add(new FreeTimeSlot(System.currentTimeMillis() / timeSlotLength + (5000/timeSlotLength), requiredTimeSlots, aid));
+				freetimeslots.add(new FreeTimeSlot((System.currentTimeMillis() - firstTimeSlot) / timeSlotLength + (5000/timeSlotLength), requiredTimeSlots, aid));
 			}
 		}
 		FreeTimeSlot freetimeslotEq = null;
