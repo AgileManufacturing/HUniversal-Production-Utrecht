cd43e176c5c2b6d4234de47d44759c9e034e5356
Duncan Jenkins <d.jenkins.work@gmail.com>
2013-09-19 14:53:01 +0200

    - Script now checks wether it is run as root - Notifies user before exiting in case it is not run as root - Script now starts the gateway server from class files instead of from a jar file - Script now searches for the users tomcat directory

--- launch-gateway-webserver.sh
+++ launch-gateway-webserver.sh
@@ -2,16 +2,22 @@
 # Assumes this file is run from the HUnivarsal-Production-Utrecht root
 # and that the gateway server jar is also present in this folder.
 
+if [ "$(id -u)" != "0" ]; then
+   echo "\033[91mThis script must be run as root\033[0m" 1>&2
+   exit 1
+fi
+
 tomcat_run() {
-	# find tomcat (TODO)
-	sudo sh ~/Programs/tomcat/bin/startup.sh
+	tcdir=$(sudo find / -type d -name "tomcat")
+	echo "\033[93mTomcat found at: $tcdir\033[0m"
+	sudo sh $tcdir/bin/startup.sh
 }
 
 msg=$(sudo netstat -lnp | grep 8080)
 if [ "$msg" = "" ];
 then
-	echo "Tomcat is not running - Starting"
+	echo "\033[91m===== Tomcat is not running - Starting =====\033[0m"
 	tomcat_run
 fi
 
-java -jar ./gatewayserver.jar
+java -classpath ./src/GatewayServer/bin Main
