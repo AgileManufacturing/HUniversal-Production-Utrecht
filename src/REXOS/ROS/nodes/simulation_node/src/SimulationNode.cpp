4e0c91e727d65233672489925ab6ba58014e514b
Garik Hakopian <info@blueheart.nl>
2013-12-18 11:12:58 +0100

    Added simulation node added stewart gough node added stewart gough object to lib still ned to do some optimalisations in these files

---
+++ src/REXOS/ROS/nodes/simulation_node/src/SimulationNode.cpp
@@ -0,0 +1,99 @@
+#include "ros/ros.h"
+
+#include "simulation_node/SimulationNode.h"
+#include <sstream>
+#include <cstdlib>
+#include "gazebo_msgs/LinkState.h"
+
+
+SimulationNode::SimulationNode()
+{
+	
+}
+
+void SimulationNode::run(int argc, char **argv){
+	
+	
+	ros::start();
+	ros::Rate loop_rate(60);
+	
+	
+	motors[0].run("upperArmToMotor1", gazeboSDF);
+	motors[1].run("upperArmToMotor2", gazeboSDF);
+	motors[2].run("upperArmToMotor3", gazeboSDF);
+	motors[3].run("upperArmToMotor4", gazeboSDF);
+	motors[4].run("upperArmToMotor5", gazeboSDF);
+	motors[5].run("upperArmToMotor6", gazeboSDF);
+	
+	//motor1.goToAngleDegrees(10);
+	//motor2.goToAngleDegrees(10);
+	//motor3.goToAngleDegrees(10);
+	//motor4.goToAngleDegrees(10);
+	//motor5.goToAngleDegrees(0);
+	//motor6.goToAngleDegrees(0);
+	
+	ros::NodeHandle nodeHandle;
+
+	ros::ServiceServer service = nodeHandle.advertiseService("rotate_motor_to_degrees", & SimulationNode::rotateMotorToDegrees, this);
+	
+	int timer = 0;
+	double degrees[11][6]{
+		{30,30,30,30,30,30},
+		{-40,-40,-40,-40,-40,-40},
+		{-10,10,-10,10,-10,10},
+		{10,-10,10,-10,10,-10},
+		{-50,-50,20,20,20,20},
+		{20,20,-50,-50,20,20},
+		{-50,-50,20,20,-50,-50},
+		{15,05,-30,-40,-30,-40},
+		{-40,-30,15,5,-30,-40},
+		{30,30,-40,-40,-40,-40},
+		{0,0,0,0,0,0}
+	};
+	int patternIndex = 0;
+	bool pattern = false;
+	ROS_INFO("=====Simulation is running======");
+	while (ros::ok())
+	{
+		
+		for(int i = 0; i < 6; i++){
+			motors[i].update();
+		}
+		
+		if(pattern == true){
+			if(timer > 100){
+				for(int i = 0; i < 6; i++){
+					motors[i].goToAngleDegrees(degrees[patternIndex][i]);
+				}
+				
+				patternIndex++;
+				if(patternIndex > 10){patternIndex = 0;}
+				timer = 0;
+			}
+			timer++;
+		}
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	
+	//ros::spin();
+}
+
+bool SimulationNode::rotateMotorToDegrees(simulation_node::rotate::Request  & req,
+									 simulation_node::rotate::Response  & res){
+  motors[req.motorIndex].goToAngleDegrees(req.degrees);
+  return true;
+}
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "Simulsation_node_client");
+	
+	SimulationNode simulationNode;
+	simulationNode.run(argc, argv);
+
+	return 0;
+}
+
+/*
+
+*/
---
+++ src/REXOS/ROS/nodes/stewart_gough/stewart_gough_node/src/StewartGoughNode.cpp
@@ -0,0 +1,403 @@
+/**
+ * @file StewartGoughNode.cpp
+ * @brief Provide the services to move the Stewart Gough robot.
+ * @date Created: 2012-09-19
+ *
+ * @author Garik Hakopian
+ *
+ * @section LICENSE
+ * License: newBSD
+ * 
+ * Copyright © 2012, HU University of Applied Sciences Utrecht.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+#include "stewart_gough_node/StewartGoughNode.h"
+#include "stewart_gough_node/Point.h"
+#include <execinfo.h>
+#include <signal.h>
+
+// @cond HIDE_NODE_NAME_FROM_DOXYGEN
+#define NODE_NAME "StewartGoughNode"
+// @endcond
+
+/**
+ * Constructor 
+ * @param equipletID identifier for the equiplet
+ * @param moduleID identifier for the deltarobot
+ **/
+stewartGoughNodeNamespace::StewartGoughNode::StewartGoughNode(int equipletID, int moduleID, std::string manufacturer, std::string typeNumber, std::string serialNumber) :
+		rexos_knowledge_database::Module(manufacturer, typeNumber, serialNumber),
+		rexos_statemachine::ModuleStateMachine("stewart_gough_node", equipletID, moduleID, true),
+		rexos_coordinates::Module(this),
+		stewartGough(NULL),
+		moduleNodeName("stewart_gough_node_" + std::to_string(equipletID) + "_" + std::to_string(moduleID)),
+		setInstructionActionServer(nodeHandle, moduleNodeName + "/set_instruction", boost::bind(&stewartGoughNodeNamespace::StewartGoughNode::onSetInstruction, this, _1), false),
+		lastX(0.0),
+		lastY(0.0),
+		lastZ(0.0){
+	ROS_INFO("StewartGoughNode Constructor entering...");
+	// get the properties and combine them for the deltarobot
+	rexos_knowledge_database::ModuleType* moduleType = this->getModuleType();
+	std::string properties = this->getModuleProperties();
+	std::string typeProperties = moduleType->getModuleTypeProperties();
+
+
+	JSONNode jsonNode = libjson::parse(properties);
+	JSONNode typeJsonNode = libjson::parse(typeProperties);
+
+	for(JSONNode::const_iterator it = typeJsonNode.begin(); it != typeJsonNode.end(); it++) {
+		jsonNode.push_back(*it);
+	}
+
+	ROS_INFO("%s", jsonNode.write_formatted().c_str());
+	
+	// Create a stewart gough robot
+	stewartGough = new rexos_stewart_gough::StewartGough(jsonNode);
+
+	setInstructionActionServer.start();
+
+	ROS_INFO_STREAM("StewartGoughNode initialized. Advertising actionserver on " << moduleNodeName << "/set_instruction");
+}
+
+
+
+stewartGoughNodeNamespace::StewartGoughNode::~StewartGoughNode() {
+	delete stewartGough;
+}
+
+
+void stewartGoughNodeNamespace::StewartGoughNode::onSetInstruction(const rexos_statemachine::SetInstructionGoalConstPtr &goal){
+	JSONNode instructionDataNode = libjson::parse(goal->json);
+	rexos_statemachine::SetInstructionResult result_;
+	result_.OID = goal->OID;
+	bool lookupIsSet = false;
+	bool movementZ = false;
+	std::string setValues;
+
+    //construct a payload
+    //construct lookupvalues.
+	Point payloadPoint, lookupResultPoint;
+	double angle, rotatedX, rotatedY;
+    JSONNode::const_iterator i = instructionDataNode.begin();
+    while (i != instructionDataNode.end()){
+        const char * nodeName = i -> name().c_str();
+	    // keep in mind that a payload may or may not contain all values. Use lastXYZ to determine these values if they are not set.
+        if (strcmp(nodeName, "payload") == 0){
+
+			payloadPoint = parsePoint(*i, &setValues);
+			lookupResultPoint = parseLookup(*i);
+			std::string angleValue = parseNodeValue("angle", *i);
+			angle = rexos_utilities::stringToDouble(angleValue);
+
+			//check whether lookup is set. If all values are 0, we can presume the lookup isnt set.
+			//Bit dangerous tho, what happends if they are all exactly 0?
+			if(!(lookupResultPoint.x == 0 && lookupResultPoint.y == 0 && angle == 0)){
+				lookupIsSet = true;
+			}
+
+			if(setValues.find("x") == -1 && setValues.find("y") == -1) {
+				// Probably a Z movement
+				payloadPoint.x = stewartGough->getEffectorLocation().x;
+				payloadPoint.y = stewartGough->getEffectorLocation().y;
+				movementZ = true;
+			}
+
+			if(setValues.find("z") == -1) {
+				// Probably a XY movement
+				payloadPoint.z = stewartGough->getEffectorLocation().z;
+			}
+
+        }
+        ++i;
+    }
+
+    Vector3 moveVector;
+    //lookup is set, so transform the (rotated) crate to a normal position.
+    if(lookupIsSet) {
+		Vector3 lookupVector(lookupResultPoint.x, lookupResultPoint.y, lookupResultPoint.z);
+
+		double theta = angle * 3.141592653589793 / 180.0;
+		double cs = cos(theta);
+		double sn = sin(theta);
+		rotatedX = payloadPoint.x * cs - payloadPoint.y * sn;
+		rotatedY = payloadPoint.x * sn + payloadPoint.y * cs;
+
+	    //translate the relative point to real equiplet coordinates.
+		Vector3 translatedVector = convertToModuleCoordinate(lookupVector);
+
+		std::cout << "[TranslatedVectorX]: \t" << translatedVector.x << "\n[TranslatedVectorY]: \t" << translatedVector.y << std::endl;
+
+		if(movementZ) {
+			// Z Movement
+			moveVector.set(payloadPoint.x, payloadPoint.y, (translatedVector.z + payloadPoint.z));
+		} else {
+			// XY Movement
+			moveVector.set((translatedVector.x + rotatedX), (translatedVector.y + rotatedY), payloadPoint.z);
+			std::cout << "[MoveVectorX]: \t" << moveVector.x << "\n[MoveVectorY]: \t" << moveVector.y << std::endl;
+		}
+
+	} else {
+		moveVector.set(payloadPoint.x, payloadPoint.y, payloadPoint.z);
+	}
+
+	//std::cout << "trying to move to x: " << moveVector.x << " y: " << moveVector.y << " z: " <<  moveVector.z << " with acceleration: " << payloadPoint.maxAcceleration << std::endl;
+	if(moveToPoint(moveVector.x, moveVector.y, moveVector.z, payloadPoint.maxAcceleration)){
+		setInstructionActionServer.setSucceeded(result_);
+		return;
+	}
+
+  	//finally move to point.
+	ROS_INFO("Failed moving to point");
+	setInstructionActionServer.setAborted(result_);
+}
+
+/**
+ * moveToPoint function, is called by the json service functions and the old deprecated service functions.
+ *
+ * @param x destination x-coordinate
+ * @param y destination y-coordinate
+ * @param z destination z-coordinate
+ * @param maxAcceleration maximum acceleration
+ * 
+ * @return false if the path is illegal, true if the motion is executed succesfully.
+ **/
+bool stewartGoughNodeNamespace::StewartGoughNode::moveToPoint(double x, double y, double z, double maxAcceleration){
+	rexos_datatypes::Point3D<double> oldLocation(stewartGough->getEffectorLocation());
+	rexos_datatypes::Point3D<double> newLocation(x,y,z);
+
+	if(stewartGough->checkPath(oldLocation, newLocation)){
+
+		stewartGough->moveTo(newLocation, maxAcceleration);
+
+		return true;
+	}
+
+	return false;
+} 
+
+/**
+ * Function that moves the stewart int that is relative to the current. 
+ *
+ * @param x destination relative x-coordinate
+ * @param y destination relative y-coordinate
+ * @param z destination relative z-coordinate
+ * @param maxAcceleration maximum acceleration
+ * 
+ * @return false if the path is illegal, true if the motion is executed succesfully.
+ **/
+bool stewartGoughNodeNamespace::StewartGoughNode::moveToRelativePoint(double x, double y, double z, double maxAcceleration){
+	rexos_datatypes::Point3D<double> oldLocation(stewartGough->getEffectorLocation());
+	rexos_datatypes::Point3D<double> newLocation(x,y,z);
+	newLocation += oldLocation;
+
+	if(stewartGough->checkPath(oldLocation, newLocation)){
+		ROS_INFO("Moving to: (%f, %f, %f) maxAcceleration=%f", x, y, z, maxAcceleration);
+		stewartGough->moveTo(newLocation, maxAcceleration);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/**
+ * Transition from Safe to Standby state
+ * @return 0 if everything went OK else error
+ **/
+void stewartGoughNodeNamespace::StewartGoughNode::transitionSetup(rexos_statemachine::TransitionActionServer* as){
+	ROS_INFO("Setup transition called");
+	// Generate the effector boundaries with voxel size 2
+	stewartGough->generateBoundaries(2);
+	// Power on the deltarobot and calibrate the motors.
+	stewartGough->powerOn();
+	// Calibrate the motors
+	if(!stewartGough->calibrateMotors()){
+		ROS_ERROR("Calibration FAILED. EXITING.");
+			as->setAborted();
+	} else {
+		as->setSucceeded();
+	}
+}
+
+/**
+ * Transition from Standby to Safe state
+ * Will turn power off the motor 
+ * @return will be 0 if everything went ok else error
+ **/
+void stewartGoughNodeNamespace::StewartGoughNode::transitionShutdown(rexos_statemachine::TransitionActionServer* as){
+	ROS_INFO("Shutdown transition called");
+	// Should have information about the workspace, calculate a safe spot and move towards it
+	stewartGough->powerOff();
+	as->setSucceeded();
+}
+
+/**
+ * Transition from Standby to Normal state
+ * @return will be 0 if everything went ok else error 
+ **/
+void stewartGoughNodeNamespace::StewartGoughNode::transitionStart(rexos_statemachine::TransitionActionServer* as){
+	ROS_INFO("Start transition called");
+	//The service servers should be set, to provide the normal methods for the equiplet
+	as->setSucceeded();
+}
+/**
+ * Transition from Normal to Standby state
+ * @return will be 0 if everything went ok else error
+ **/
+void stewartGoughNodeNamespace::StewartGoughNode::transitionStop(rexos_statemachine::TransitionActionServer* as){
+	ROS_INFO("Stop transition called");
+	//The service servers should be set off, so the equiplet isn't able to set tasks for the module
+		as->setSucceeded();
+	// Go to base (Motors on 0 degrees)
+}
+
+/**
+ * Parse a JSON string to a Point object
+ *
+ * @param json String that contains the json thats need to be parsed to a point
+ *
+ * @return Point object that is initialized from the data in the JSON
+ **/
+stewartGoughNodeNamespace::Point stewartGoughNodeNamespace::StewartGoughNode::parsePoint(const JSONNode & n, std::string * valuesSet){
+
+	JSONNode::const_iterator i = n.begin();
+	Point p;
+
+	while(i != n.end()){
+		// get the JSON node name and value as a string
+		std::string node_name = i->name();
+
+		if(node_name == "x") {
+			p.x = i->as_float();
+			*valuesSet += "x";
+		} else if(node_name == "y") {
+			p.y = i->as_float();
+			*valuesSet += "y";
+		} else if(node_name == "z") {
+			p.z = i->as_float();
+			*valuesSet += "z";
+		} else if(node_name == "maxAcceleration"){
+			p.maxAcceleration = i->as_float();
+		}
+
+		++i;
+	}
+	
+	return p;
+}
+
+stewartGoughNodeNamespace::Point stewartGoughNodeNamespace::StewartGoughNode::parseLookup(const JSONNode & n){
+
+	Point p;
+	JSONNode::const_iterator i = n.begin();
+	while(i != n.end()) {
+		// get the JSON node name and value as a string
+		std::string node_name = i->name();
+		if(node_name == "locationX") {
+			p.x = i->as_float();
+		} else if(node_name == "locationY") {
+			p.y = i->as_float();
+		} else if(node_name == "locationZ") {
+			p.z = i->as_float();
+		}
+		++i;
+	}
+
+	return p;
+}
+
+std::string stewartGoughNodeNamespace::StewartGoughNode::parseNodeValue(const std::string nodeName, const JSONNode & n){
+
+	JSONNode::const_iterator i = n.begin();
+	std::string result;
+	while(i != n.end()){
+		// get the JSON node name and value as a string
+		std::string node_name = i->name();
+		if(node_name == nodeName)
+		{
+			result = i->as_string();
+		} 
+		++i;
+	}
+	return result;
+}
+
+
+/**
+ * Parse a JSON string to a Point object
+ *
+ * @param json String that contains the json thats need to be parsed to a point
+ * @param size Reference to an int size, caller of the function needs to know this
+ *
+ * @return Point object that is initialized from the data in the JSON
+ **/
+stewartGoughNodeNamespace::Point* stewartGoughNodeNamespace::StewartGoughNode::parsePointArray(std::string json, int &size){
+	ROS_INFO("Parsing JSON Array");
+	std::string discarded;
+	JSONNode pathArray = libjson::parse(json);
+	Point *path = new Point[pathArray.size()];
+	int counter = 0;
+
+	JSONNode::const_iterator i = pathArray.begin();
+	while(i != pathArray.end()){
+		Point p = parsePoint(*i, &discarded);
+		path[counter++] = p;
+		++i;
+	}
+
+	size = pathArray.size();
+	ROS_INFO("The size of the array %d", size);
+	for(int i = 0; i < (int)size; i++){
+		ROS_INFO("z value of item %d in array on position %d", (int)path[0].z, size);
+	}
+	ROS_INFO("Done parsing JSON Array");
+	return path;
+}
+
+/**
+ * Main that creates the deltaRobotNode and starts the statemachine
+ **/
+int main(int argc, char **argv){
+	ros::init(argc, argv, NODE_NAME);
+	
+	if(argc < 6){
+		ROS_ERROR("Usage: stewart_gough_node equipletId, moduleId, manufacturer, typeNumber, serialNumber");
+		return -1;
+	}
+	
+	
+	int equipletID;
+	int moduleID;
+	try{
+		equipletID = rexos_utilities::stringToInt(argv[1]);
+		moduleID = rexos_utilities::stringToInt(argv[2]);
+	} catch(std::runtime_error ex) {
+		ROS_ERROR("Cannot read equiplet id and/or moduleId from commandline please use correct values.");
+		return -2;
+	}
+	
+	ROS_INFO("Creating StewartGoughNode");
+
+	stewartGoughNodeNamespace::StewartGoughNode drn(equipletID, moduleID, argv[3], argv[4], argv[5]);
+
+	ROS_INFO("Running StateEngine");
+	ros::spin();
+	return 0;
+}
