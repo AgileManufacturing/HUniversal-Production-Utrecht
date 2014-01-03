a82fce86823fc4b1568c93bc5295c49bfecd8aaf
AlexanderHustinx <alexanderhustinx@gmail.com>
2014-01-03 15:19:11 +0100

    Demo.java:     Work in progress/testing for the visual aspect of the grid EquipletListFrame.java:     GUI-window containing a list of all Equiplets ( stub ) FileCheckerThread.java:     Class that checks if the required files are present, and keeps updating them MainGUI.java:     Main window of the GUI (Work in progress, waiting for Duncan's part and more) ProgressWorkerThread.java:     Class that updates the status-bar of the GUI SimGUI.java:     The so called "main.java" that starts the GUI ( Depricate ) TableUpdateThread.java:    Updates the Table in the EquipletListFrame DrawGrid.java:     Class that draws all the Visual aspect of the Simulation/Grid (Work in progress) Equiplet.java:     Work in progress / temp Grid.java:     Work in progress / temp

---
+++ src/REXOS/MAS/simulation/gui/ProgressWorkerThread.java
@@ -0,0 +1,94 @@
+/**                                     ______  _______   __ _____  _____
+ *                  ...++,              | ___ \|  ___\ \ / /|  _  |/  ___|
+ *                .+MM9WMMN.M,          | |_/ /| |__  \ V / | | | |\ `--.
+ *              .&MMMm..dM# dMMr        |    / |  __| /   \ | | | | `--. \
+ *            MMMMMMMMMMMM%.MMMN        | |\ \ | |___/ /^\ \\ \_/ //\__/ /
+ *           .MMMMMMM#=`.gNMMMMM.       \_| \_|\____/\/   \/ \___/ \____/
+ *             7HMM9`   .MMMMMM#`		
+ *                     ...MMMMMF .      
+ *         dN.       .jMN, TMMM`.MM     	@file 	ProgressWorkerThread.java
+ *         .MN.      MMMMM;  ?^ ,THM		@brief 	...
+ *          dM@      dMMM3  .ga...g,    	@date Created:	2013-12-18
+ *       ..MMM#      ,MMr  .MMMMMMMMr   
+ *     .dMMMM@`       TMMp   ?TMMMMMN   	@author	Alexander Hustinx
+ *   .dMMMMMF           7Y=d9  dMMMMMr    
+ *  .MMMMMMF        JMMm.?T!   JMMMMM#		@section LICENSE
+ *  MMMMMMM!       .MMMML .MMMMMMMMMM#  	License:	newBSD
+ *  MMMMMM@        dMMMMM, ?MMMMMMMMMF    
+ *  MMMMMMN,      .MMMMMMF .MMMMMMMM#`    	Copyright © 2013, HU University of Applied Sciences Utrecht. 
+ *  JMMMMMMMm.    MMMMMM#!.MMMMMMMMM'.		All rights reserved.
+ *   WMMMMMMMMNNN,.TMMM@ .MMMMMMMM#`.M  
+ *    JMMMMMMMMMMMN,?MD  TYYYYYYY= dM     
+ *                                        
+ *	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *	- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *	- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *	- Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *   ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+ *   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JComponent;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+
+class ProgressWorkerThread extends SwingWorker<Void, Void> {
+	
+	public static final int PLACEHOLDER = 0;
+	
+	private MainGUI mG;
+	private JComponent[] components;
+	DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	
+	int i;
+	
+	public ProgressWorkerThread(MainGUI mG){
+		super();
+		this.mG = mG;
+		components = mG.getProgressComponents();
+		System.out.println("[DEBUG]\t\tCreated ProgressWorkerThread");
+		i = 0;
+	}
+	
+	@Override
+	public Void doInBackground() {
+		while(!isCancelled()){
+			//@ TODO Update progressBar with Sim-data
+			Date date = new Date();
+			
+			i++;
+			if(i >= 100)	i = 0;
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			((JProgressBar) components[0]).setValue(PLACEHOLDER+i);
+			((JTextField) 	components[1]).setText(dateFormat.format(date));
+
+			mG.setProgressComponents(components);
+		}
+		return null;
+	}
+	
+	protected void done(){
+		System.out.println("[DEBUG]\t\tCancelled ProgressWorkerThread");
+	}
+}
+
