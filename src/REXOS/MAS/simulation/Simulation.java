7201f120ea5a62dfad2bd0388e256d2248ba8b36
tommasbakker <tommas.bakker@student.hu.nl>
2014-01-13 19:04:07 +0100

    Manual merge

--- src/REXOS/MAS/simulation/Simulation.java
+++ src/REXOS/MAS/simulation/Simulation.java
@@ -50,42 +50,98 @@ import simulation.mas_entities.Grid;
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class Simulation implements Runnable{
-	
+	/** 
+	 * boolean indicating whether the simulation is currently running 
+	 */
 	private boolean isRunning = false;
+	/**
+	 * boolean indicating whether the simulation has been aborted. The simulation can be aborted by calling abort()
+	 */
 	private boolean isAborted = false;
+	/**
+	 * boolean indicating whether the simulation has finished successfully.
+	 */
 	private boolean isFinished = false;
+	/**
+	 * The interval in seconds between ticks. 
+	 * Normally set to 0 (for maximum simulation speed). Adjust when running a realtime simulation.
+	 */
 	private double interval = 0.00; // seconds
 	private double turnTime = 0.1; // seconds
-	private double duration = 4 * 60 * 60 * 1000 + 0 * 60 * 1000; // milliseconds
+	private double duration = 2 * 60 * 60 * 1000 + 0 * 60 * 1000; // milliseconds
 	
-	private long turn = 0;
+	/**
+	 * The current tick number of the simulation. Starts at 0
+	 */
+	private long tick = 0;
+	/**
+	 * The system time in milliseconds at the moment that the simulation started
+	 */
 	private long startSimulationTime;
+	/**
+	 * The current simulation time in milliseconds. Should be equal to startSimulationTime + tick * tickTime
+	 */
 	private long currentSimulationTime;
+	/**
+	 * A list of all the updatables that should be updated every tick
+	 */
 	private ArrayList<Updatable> updatables = new ArrayList<Updatable>();
+	/**
+	 * A list of all the updatables that should be added. The simulation adds these updatables at the end of each tick. 
+	 */
 	private ArrayList<Updatable> updatablesToBeAdded = new ArrayList<Updatable>();
+	/**
+	 * A list of all the updatables that should be removed. The simulation adds these updatables at the end of each tick.
+	 */
 	private ArrayList<Updatable> updatablesToBeRemoved = new ArrayList<Updatable>();
+	/**
+	 * A list of all the datacollectors that should be updated
+	 */
 	private ArrayList<DataCollector> dataCollectors = new ArrayList<DataCollector>();
+	/**
+	 * A list of all the datacollectors that should be added. The simulation adds these datacollectors at the end of each tick. 
+	 */
 	private ArrayList<DataCollector> dataCollectorsToBeAdded = new ArrayList<DataCollector>();
+	/**
+	 * The thread used to run the simulation
+	 */
 	private Thread thread;
 	
+	/**
+	 * The grid for this simulation. Should be removed?
+	 */
 	public Grid grid;
 	
+	/**
+	 * Pauses the simulation. The simulation will finish the current tick and will halt before executing the next tick.
+	 */
 	public void pauseSimulation(){
 		isRunning = false;
 	}
+	/**
+	 * Resumes the simulation.
+	 */
 	public synchronized void resumeSimulation() {
 		isRunning = true;
 		notify();
 	}
+	/**
+	 * Aborts the simulation
+	 */
 	public synchronized void abort() {
 		isAborted = true;
 	}
-	
+	/**
+	 * This method will wait until the simulation thread has died (which happens when the simulation is either aborted of finished).
+	 * @throws Exception if the simulation is currently paused.
+	 */
 	public void waitUntilFinished() throws Exception{
 		if(isRunning == false) throw new Exception("Pointsless to wait on a simulation which is not running");
 		thread.join();
 	}
-	
+	/**
+	 * Constructs the simulation and starts the thread but does NOT start the simulation
+	 */
 	public Simulation() {
 		startSimulationTime = System.currentTimeMillis();
 		currentSimulationTime = startSimulationTime;
@@ -101,26 +157,40 @@ public class Simulation implements Runnable{
 	public synchronized long getStartSimulationTime() {
 		return startSimulationTime;
 	}
-	
+	/**
+	 * Add an updateable to the simulation. The updateable will be added at the end of the tick
+	 * @param updateable
+	 */
 	public synchronized void addUpdateable(Updatable updateable) {
 		updatablesToBeAdded.add(updateable);
 	}
 	
+	/**
+	 * Add an dataCollector to the simulation. The dataCollector will be added at the end of the tick
+	 * @param dataCollector
+	 */
 	public synchronized void addDataCollector(DataCollector dataCollector) {
 		dataCollectorsToBeAdded.add(dataCollector);
 	}
 	
+	/**
+	 * Get the progress of the simulation
+	 * @return the progress with [0.0, 1.0]
+	 */
 	public synchronized double getProgress(){
 		return (currentSimulationTime - startSimulationTime) / duration;
 	}
 	
+	/**
+	 * The method for the simulation thread. Do NOT call!
+	 */
 	public void run(){
 		while(isAborted == false) {
 			synchronized (this) {
 				long cycleStartTime = System.currentTimeMillis();
 				if(isRunning == false) {
 					try {
-						System.out.println("Simulation halted on turn " + turn);
+						System.out.println("Simulation halted on tick " + tick);
 						wait();
 					} catch (InterruptedException e) {
 						// ignore, this is expected
@@ -142,8 +212,8 @@ public class Simulation implements Runnable{
 				}
 				
 				// update the time
-				currentSimulationTime += turnTime * 1000;
-				turn++;
+				currentSimulationTime += tickTime * 1000;
+				tick++;
 				
 				// sleep if dealing with 'realtime' simulation
 				long cycleEndTime = System.currentTimeMillis();
@@ -180,12 +250,25 @@ public class Simulation implements Runnable{
 		}
 		// exit simulation thread
 	}
+	
+	/**
+	 * Get the updatables currently in the simulation (without the updatables to be added and with the updatables to be removed)
+	 * @return an array of updatables
+	 */
 	public Updatable[] getUpdatables() {
 		return updatables.toArray(new Updatable[updatables.size()]);
 	}
+	/**
+	 * Temoves the updatable to the list of updatables by first adding it to the list of updatablesToBeRemoved
+	 * @param updateable
+	 */
 	public void removeUpdateable(Updatable updateable) {
 		updatablesToBeRemoved.add(updateable);
 	}
+	/**
+	 * returns true if the simulation is finished
+	 * @return
+	 */
 	public boolean getIsFinished() {
 		return isFinished;
 	}
