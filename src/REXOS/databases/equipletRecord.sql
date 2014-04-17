9d5b77393d10b760f42053334b0e18e33ef0d465
tommasbakker <tommas.bakker@student.hu.nl>
2014-04-17 17:00:46 +0200

    Added equiplet record entry for testing purposes

---
+++ src/REXOS/databases/equipletRecord.sql
@@ -0,0 +1,11 @@
+insert into JavaSoftware
+(buildNumber, jarFile, className)
+values(1, "", "equiplet");
+
+insert into RosSoftware
+(buildNumber, jarFile, className)
+values(1, "", "equiplet");
+
+insert into Equiplet
+(name, mountPointsX, mountPointsY, mountPointDistanceX, mountPointDistanceY, rosSoftware, masSoftware)
+values("EQ1", 10, 10, 50, 50, 1, 1);
