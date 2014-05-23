15b24be72fed3d2ff2e17f6af7263d312db8d4ee
tommasbakker <tommas.bakker@student.hu.nl>
2014-05-23 22:19:08 +0200

    Removed deprecated files in root

--- VISIONdevelopment_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ VISIONdevelopment - Debug ]----------
-	@"$(MAKE)" -f "VISIONdevelopment.mk"
-clean:
-	@echo ----------Cleaning project:[ VISIONdevelopment - Debug ]----------
-	@"$(MAKE)" -f "VISIONdevelopment.mk" clean
