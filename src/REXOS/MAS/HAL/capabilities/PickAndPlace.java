de1cc55b1ee7c676d2a3cdb2024d9b5ccdc3dcaf
Aristides Ayala Mendoza <ari.ayalamendoza@student.hu.nl>
2014-05-27 15:10:33 +0200

    Added HALTesterClassPickAndPlace

--- src/REXOS/MAS/HAL/capabilities/PickAndPlace.java
+++ src/REXOS/MAS/HAL/capabilities/PickAndPlace.java
@@ -39,7 +39,6 @@ import java.util.ArrayList;
 import libraries.dynamicloader.JarFileLoaderException;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
