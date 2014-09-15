77b8ca2f2d54d7e125145c01ec9874b2ed70fcf4
madmilla <madmilla@gmail.com>
2014-09-15 16:25:55 +0200

    Updated ANT to build correctly. Added to ANT: EMUREX which now can be build and run from the command-line.

---
+++ simulation/simulation_launch.sh
@@ -0,0 +1,2 @@
+java -cp simulation.jar:../src/REXOS/external_libraries/jade.jar:../src/REXOS/external_libraries/java-json.jar:../src/REXOS/external_libraries/jfreechart-1.0.17.jar:../src/REXOS/external_libraries/jcommon-1.0.21.jar:../src/REXOS/external_libraries/commons-lang-2.3.jar jade.Boot -agents simulation:MAS.simulation.simulation.SimulationAgent -verbosity 0
+
