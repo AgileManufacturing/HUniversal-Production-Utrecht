734326f9c31b25abfc7b202cadb03f4f959d13b4
tommasbakker <tommas.bakker@student.hu.nl>
2014-12-02 00:03:30 +0100

    Modified the HAL tester classes

--- src/REXOS/HAL/testerClasses/HALTesterClass.java
+++ src/REXOS/HAL/testerClasses/HALTesterClass.java
@@ -15,6 +15,7 @@ import util.log.Logger;
 import HAL.BlackboardHandler;
 import HAL.HardwareAbstractionLayer;
 import HAL.Module;
+import HAL.ModuleIdentifier;
 import HAL.listeners.HardwareAbstractionLayerListener;
 import HAL.steps.HardwareStep;
 import HAL.steps.HardwareStep.HardwareStepStatus;
@@ -29,273 +30,71 @@ public class HALTesterClass implements HardwareAbstractionLayerListener {
 	static HardwareAbstractionLayer hal;
 	static BlackboardHandler blackboardUpdated;
 	
-	//static final String baseDir = "/home/t/Desktop/"; 		// <-- Tommas
+	static final String baseDir = "/home/t/Desktop/"; 		// <-- Tommas
 	//static final String baseDir = "/home/cm-storm2/lib/"; 		// <-- Workmachine lars
-	static final String baseDir = "/home/peter/lib/";
+	//static final String baseDir = "/home/peter/lib/";
 	
-	// delta robot
+	// dummy module A
 	static String moduleA_01 = "{"
 			+ "	\"manufacturer\":\"HU\","
-			+ "	\"typeNumber\":\"delta_robot_type_B\","
+			+ "	\"typeNumber\":\"dummy_module_type_A\","
 			+ "	\"serialNumber\":\"1\","
 			+ "	\"type\":{"
-			+ "		\"properties\":\"{"
-			+ "	\\\"midPointX\\\" : 75.0,"
-			+ "	\\\"midPointY\\\" : -200.0,"
-			+ "	\\\"midPointZ\\\" : -35.3,"
-			+ "	\\\"deltaRobotMeasures\\\" : {"
-			+ "		\\\"baseRadius\\\" : 101.3,"
-			+ "		\\\"hipLength\\\" : 100.0,"
-			+ "		\\\"effectorRadius\\\" : 46.19,"
-			+ "		\\\"ankleLength\\\" : 300.0,"
-			+ "		\\\"hipAnleMaxAngleDegrees\\\" : 22.0,"
-			+ "		\\\"motorFromZeroToTopAngleDegrees\\\" : 20.0,"
-			+ "		\\\"boundaryBoxMinX\\\" : -200.0,"
-			+ "		\\\"boundaryBoxMaxX\\\" : 200.0,"
-			+ "		\\\"boundaryBoxMinY\\\" : -200.0,"
-			+ "		\\\"boundaryBoxMaxY\\\" : 200.0,"
-			+ "		\\\"boundaryBoxMinZ\\\" : -380.0,"
-			+ "		\\\"boundaryBoxMaxZ\\\" : -180.0"
-			+ "	},"
-			+ "	\\\"calibrationBigStepFactor\\\" : 20,"
-			+ "	\\\"stepperMotorProperties\\\" : {"
-			+ "		\\\"motorMinAngleDegrees\\\" : -18.0,"
-			+ "		\\\"motorMaxAngleDegrees\\\" : 90.0,"
-			+ "		\\\"microStepAngleDegrees\\\" : 0.036,"
-			+ "		\\\"minAccelerationDegrees\\\" : 36,"
-			+ "		\\\"maxAccelerationDegrees\\\" : 36000,"
-			+ "		\\\"minSpeedDegrees\\\" : 0.036,"
-			+ "		\\\"maxSpeedDegrees\\\" : 18000"
-			+ "	}"
-			+ "}\","
+			+ "		\"properties\":\"{}\","
 			+ "		\"rosSoftware\":{"
 			+ "			\"buildNumber\":1,"
 			+ "			\"rosFile\": \"";
 	static String moduleA_02 = "\","
-			+ "			\"command\":\"rosrun delta_robot_node delta_robot_node {equipletName} {manufacturer} {typeNumber} {serialNumber}\""
+			+ "			\"command\":\"rosrun dummy_module_a dummy_module_a {equipletName} {manufacturer} {typeNumber} {serialNumber}\""
 			+ "		},"
 			+ "		\"halSoftware\":{"
 			+ "			\"buildNumber\":1,"
 			+ "			\"jarFile\": \"";
 	static String moduleA_03 = "\","
-			+ "			\"className\":\"HAL.modules.DeltaRobot\""
+			+ "			\"className\":\"HAL.modules.DummyModuleA\""
 			+ "		},"
 			+ "		\"supportedMutations\": ["
-			+ "			\"move\""
 			+ "		],"
-			+ "		\"capabilities\":["
-			+ "			{"
-			+ "				\"name\":\"Draw\","
-			+ "				\"treeNumber\":1,"
-			+ "				\"halSoftware\":{"
-			+ "					\"buildNumber\":1,"
-			+ "					\"jarFile\": \"";
-	static String moduleA_04 = "\","
-			+ "					\"className\":\"HAL.capabilities.Draw\""
-			+ "				},"
-			+ "				\"requiredMutationsTrees\":["
-			+ "					{"
-			+ "						\"treeNumber\":1,"
-			+ "						\"mutations\":["
-			+ "							\"move\", \"draw\""
-			+ "						]"
-			+ "					}"
-			+ "				],"
-			+ "				\"services\":["
-			+ "					\"draw\""
-			+ "				]"
-			+ "			},"
-			+ "			{"
-			+ "				\"name\":\"PickAndPlace\","
-			+ "				\"treeNumber\":1,"
-			+ "				\"halSoftware\":{"
-			+ "					\"buildNumber\":1,"
-			+ "					\"jarFile\": \"";
-	static String moduleA_05 = "\","
-			+ "					\"className\":\"HAL.capabilities.PickAndPlace\""
-			+ "				},"
-			+ "				\"requiredMutationsTrees\":["
-			+ "					{"
-			+ "						\"treeNumber\":1,"
-			+ "						\"mutations\":["
-			+ "							\"move\", \"pick\", \"place\""
-			+ "						]"
-			+ "					}"
-			+ "				],"
-			+ "				\"services\":["
-			+ "					\"place\""
-			+ "				]"
-			+ "			}"
-			+ "		]"
+			+ "		\"capabilities\":[]"
 			+ "	},"
-			+ "	\"properties\":\"{"
-			+ "	\\\"modbusIp\\\" : \\\"192.168.0.22\\\","
-			+ "	\\\"modbusPort\\\" : 502"
-			+ "}\","
+			+ "	\"properties\":\"{}\","
 			+ "	\"calibrationData\":["
-			+ "		{"
-			+ "			\"date\":\"2014-01-01\","
-			+ "			\"data\":\"aapkip\","
-			+ "			\"moduleSet\":["
-			+ "				{"
-			+ "					\"manufacturer\":\"manA\","
-			+ "					\"typeNumber\":\"typeA\","
-			+ "					\"serialNumber\":\"serA\""
-			+ "				}"
-			+ "			]"
-			+ "		}"
 			+ "	],"
 			+ "	\"attachedTo\":null,"
 			+ "\"mountPointX\":3,"
 			+ "\"mountPointY\":2"
 			+ "}";
-	// pen
+	// dummy module B
 	static String moduleB_01 = "{"
 			+ "	\"manufacturer\":\"HU\","
-			+ "	\"typeNumber\":\"gripper_type_A\","
+			+ "	\"typeNumber\":\"dummy_module_type_B\","
 			+ "	\"serialNumber\":\"1\","
 			+ "	\"type\":{"
-			+ "		\"properties\":\"{"
-			+ "	\\\"modbusAddress\\\" : 8001,"
-			+ "	\\\"modbusDevicePin\\\" : 0,"
-			+ " \\\"gripperSize\\\" : 46.54,"
-			+ "	\\\"gripperEnabledMaxSeconds\\\" : 60,"
-			+ "	\\\"gripperEnabledWarningSeconds\\\" : 50,"
-			+ "	\\\"gripperEnabledCooldownSeconds\\\" : 180,"
-			+ "	\\\"watchdogInterval\\\" : 100"
-			+ "}\","
+			+ "		\"properties\":\"{}\","
 			+ "		\"rosSoftware\":{"
 			+ "			\"buildNumber\":1,"
 			+ "			\"rosFile\": \"";
 	static String moduleB_02 = "\","
-			+ "			\"command\":\"rosrun gripper_node gripper_node {equipletName} {manufacturer} {typeNumber} {serialNumber}\""
+			+ "			\"command\":\"rosrun dummy_module_b dummy_module_b {equipletName} {manufacturer} {typeNumber} {serialNumber}\""
 			+ "		},"
 			+ "		\"halSoftware\":{"
 			+ "			\"buildNumber\":1,"
 			+ "			\"jarFile\": \"";
 	static String moduleB_03 = "\","
-			+ "			\"className\":\"HAL.modules.Gripper\""
-			+ "		},"
-			+ "		\"supportedMutations\": ["
-			+ "			\"pick\", \"place\""
-			+ "		],"
-			+ "		\"capabilities\":["
-			+ "		]"
-			+ "	},"
-			+ "	\"properties\":\"name\","
-			+ "	\"calibrationData\":["
-			+ "	],"
-			+ "	\"attachedTo\":{"
-			+ "		\"manufacturer\":\"HU\","
-			+ "		\"typeNumber\":\"delta_robot_type_B\","
-			+ "		\"serialNumber\":\"1\""
-			+ "	},"
-			+ "\"mountPointX\":null,"
-			+ "\"mountPointY\":null"
-			+ "}";
-	// camera
-	static String moduleC_01 = "{"
-			+ "	\"manufacturer\":\"The_Imaging_Source_Europe_GmbH\","
-			+ "	\"typeNumber\":\"DFK_22AUC03\","
-			+ "	\"serialNumber\":\"1\","
-			+ "	\"type\":{"
-			+ "		\"properties\":\"\","
-			+ "		\"rosSoftware\":{"
-			+ "			\"buildNumber\":1,"
-			+ "			\"rosFile\": \"";
-	static String moduleC_02 = "\","
-			+ "			\"command\":\"roslaunch camera.launch equipletName:={equipletName} manufacturer:={manufacturer} typeNumber:={typeNumber} serialNumber:={serialNumber}\""
-			+ "		},"
-			+ "		\"halSoftware\":{"
-			+ "			\"buildNumber\":1,"
-			+ "			\"jarFile\": \"";
-	static String moduleC_03 = "\","
-			+ "			\"className\":\"HAL.modules.Camera\""
+			+ "			\"className\":\"HAL.modules.DummyModuleB\""
 			+ "		},"
 			+ "		\"supportedMutations\": ["
 			+ "		],"
-			+ "		\"capabilities\":["
-			+ "		]"
+			+ "		\"capabilities\":[]"
 			+ "	},"
-			+ "	\"properties\":\"\","
+			+ "	\"properties\":\"{}\","
 			+ "	\"calibrationData\":["
 			+ "	],"
 			+ "	\"attachedTo\":null,"
 			+ "\"mountPointX\":3,"
-			+ "\"mountPointY\":16"
+			+ "\"mountPointY\":4"
 			+ "}";
-	// lens
-	static String moduleD_01 = "{"
-			+ "	\"manufacturer\":\"The_Imaging_Source_Europe_GmbH\","
-			+ "	\"typeNumber\":\"Cheap_ass_lens\","
-			+ "	\"serialNumber\":\"1\","
-			+ "	\"type\":{"
-			+ "		\"properties\":\"\","
-			+ "		\"rosSoftware\":null,"
-			+ "		\"halSoftware\":{"
-			+ "			\"buildNumber\":1,"
-			+ "			\"jarFile\": \"";
-	static String moduleD_02 = "\","
-			+ "			\"className\":\"HAL.modules.Lens\""
-			+ "		},"
-			+ "		\"supportedMutations\": ["
-			+ "		],"
-			+ "		\"capabilities\":["
-			+ "		]"
-			+ "	},"
-			+ "	\"properties\":\"\","
-			+ "	\"calibrationData\":["
-			+ "	],"
-			+ "	\"attachedTo\":{"
-			+ "		\"manufacturer\":\"The_Imaging_Source_Europe_GmbH\","
-			+ "		\"typeNumber\":\"DFK_22AUC03\","
-			+ "		\"serialNumber\":\"1\""
-			+ "	},"
-			+ "\"mountPointX\":null,"
-			+ "\"mountPointY\":null"
-			+ "}";
-	// workplane
-	static String moduleE_01 = "{"
-			+ "     \"manufacturer\":\"HU\","
-			+ "     \"typeNumber\":\"workplane_type_A\","
-			+ "     \"serialNumber\":\"1\","
-			+ "     \"type\":{"
-			+ "	     \"properties\":\"{"
-			+ "     \\\"midPointX\\\" : 175.0,"
-			+ "     \\\"midPointY\\\" : -200.0,"
-			+ "     \\\"midPointZ\\\" : 33.33,"
-			+ "     \\\"topLeftValue\\\" : \\\"_WP_TL\\\","
-			+ "     \\\"topRightValue\\\" : \\\"_WP_TR\\\","
-			+ "     \\\"bottomRightValue\\\" : \\\"_WP_BR\\\","
-			+ "     \\\"workPlaneWidth\\\" : 80.0,"
-			+ "     \\\"workPlaneHeight\\\" : 80.0"
-			+ "}\","
-			+ "	     \"rosSoftware\":{"
-			+ "		     \"buildNumber\":1,"
-			+ "		     \"rosFile\": \"";
-	static String moduleE_02 = "\","
-			+ "		     \"command\":\"rosrun part_locator_node part_locator_node {equipletName} {manufacturer} {typeNumber} {serialNumber}\""
-			+ "	     },"
-			+ "	     \"halSoftware\":{"
-			+ "		     \"buildNumber\":1,"
-			+ "		     \"jarFile\": \"";
-	static String moduleE_03 = "\","
-			+ "		     \"className\":\"HAL.modules.Workplane\""
-			+ "	     },"
-			+ "	     \"supportedMutations\": ["
-			+ "	     ],"
-			+ "	     \"capabilities\":["
-			+ "	     ]"
-			+ "     },"
-			+ "     \"properties\":\"\","
-			+ "     \"calibrationData\":["
-			+ "     ],"
-			+ "     \"attachedTo\":null,"
-			+ "\"mountPointX\":1,"
-			+ "\"mountPointY\":10"
-			+ "}";
-
-			
+	
 	/**
 	 * @param args
 	 * @throws Exception 
@@ -308,128 +107,53 @@ public class HALTesterClass implements HardwareAbstractionLayerListener {
 		FileInputStream fis;
 		byte[] content;
 
-		File deltaRobotJar = new File(baseDir + "DeltaRobot.jar");
-		fis = new FileInputStream(deltaRobotJar);
-		content = new byte[(int) deltaRobotJar.length()];
+		File dummyModuleAJar = new File(baseDir + "DummyModuleA.jar");
+		fis = new FileInputStream(dummyModuleAJar);
+		content = new byte[(int) dummyModuleAJar.length()];
 		fis.read(content);
 		fis.close();
-		String base64DeltaRobot = new String(Base64.encodeBase64(content));
+		String base64DummyModuleA = new String(Base64.encodeBase64(content));
 		
-		File deltaRobotZip = new File(baseDir + "nodes.zip");
-		fis = new FileInputStream(deltaRobotZip);
-		content = new byte[(int) deltaRobotZip.length()];
+		File dummyModuleBJar = new File(baseDir + "DummyModuleB.jar");
+		fis = new FileInputStream(dummyModuleBJar);
+		content = new byte[(int) dummyModuleBJar.length()];
 		fis.read(content);
 		fis.close();
-		String base64DeltaRobotRos = new String(Base64.encodeBase64(content));
+		String base64DummyModuleB = new String(Base64.encodeBase64(content));
 		
-		File gripperZip = new File(baseDir + "nodes.zip");
-		fis = new FileInputStream(gripperZip);
-		content = new byte[(int) gripperZip.length()];
+		File dummyModuleAZip = new File(baseDir + "nodes.zip");
+		fis = new FileInputStream(dummyModuleAZip);
+		content = new byte[(int) dummyModuleAZip.length()];
 		fis.read(content);
 		fis.close();
-		String base64GripperRos = new String(Base64.encodeBase64(content));
+		String base64DummyModuleARos = new String(Base64.encodeBase64(content));
 		
-		File cameraZip = new File(baseDir + "nodes.zip");
-		fis = new FileInputStream(cameraZip);
-		content = new byte[(int) cameraZip.length()];
+		File dummyModuleBZip = new File(baseDir + "nodes.zip");
+		fis = new FileInputStream(dummyModuleBZip);
+		content = new byte[(int) dummyModuleBZip.length()];
 		fis.read(content);
 		fis.close();
-		String base64CameraRos = new String(Base64.encodeBase64(content));
+		String base64DummyModuleBRos = new String(Base64.encodeBase64(content));
 		
-		File workplaneZip = new File(baseDir + "nodes.zip");
-		fis = new FileInputStream(workplaneZip);
-		content = new byte[(int) workplaneZip.length()];
-		fis.read(content);
-		fis.close();
-		String base64WorkplaneRos = new String(Base64.encodeBase64(content));
-		
-		File penJar = new File(baseDir + "Pen.jar");
-		
-		fis = new FileInputStream(penJar);
-		content = new byte[(int) penJar.length()];
-		fis.read(content);
-		fis.close();
-		String base64Pen = new String(Base64.encodeBase64(content));
-		
-		File gripperJar = new File(baseDir + "Gripper.jar");
-		fis = new FileInputStream(gripperJar);
-		content = new byte[(int) gripperJar.length()];
-		fis.read(content);
-		fis.close();
-		String base64Gripper = new String(Base64.encodeBase64(content));
-		
-		File drawJar = new File(baseDir + "Draw.jar");
-		fis = new FileInputStream(drawJar);
-		content = new byte[(int) drawJar.length()];
-		fis.read(content);
-		fis.close();
-		String base64Draw = new String(Base64.encodeBase64(content));
-		
-		File pickAndPlaceJar = new File(baseDir + "PickAndPlace.jar");
-		fis = new FileInputStream(pickAndPlaceJar);
-		content = new byte[(int) pickAndPlaceJar.length()];
-		fis.read(content);
-		fis.close();
-		String base64PickAndPlace = new String(Base64.encodeBase64(content));
-		
-		
-		// deltarobot
-		String moduleA = moduleA_01 + base64DeltaRobotRos + moduleA_02 + base64DeltaRobot + moduleA_03 + base64Draw + moduleA_04 + base64PickAndPlace + moduleA_05; 
+		// dummy module A
+		String moduleA = moduleA_01 + base64DummyModuleARos + moduleA_02 + base64DummyModuleA + moduleA_03;
 		JSONObject a = new JSONObject(new JSONTokener(moduleA));
-		hal.insertModule(a, a);
 		
-		// pen
-		//String moduleB = moduleB_01 + base64Pen + moduleB_02; 
-		//JSONObject b = new JsonParser().parse(moduleB).getAsJSONObject();
+		// dummy module B
+		String moduleB = moduleB_01 + base64DummyModuleBRos + moduleB_02 + base64DummyModuleB + moduleB_03;
+		JSONObject b = new JSONObject(new JSONTokener(moduleB));
+		
+		
+		//hal.insertModule(a, a);
 		//hal.insertModule(b, b);
 		
-		// gripper
-		String moduleB = moduleB_01 + base64GripperRos + moduleB_02 + base64Gripper + moduleB_03; 
-		JSONObject b = new JSONObject(new JSONTokener(moduleB));
-		hal.insertModule(b, b);
-		
-		// camera
-		String moduleC = moduleC_01 + base64CameraRos + moduleC_02 + base64Pen + moduleC_03;
-		JSONObject c = new JSONObject(new JSONTokener(moduleC));
-		hal.insertModule(c, c);
-		
-		// lens
-		String moduleD = moduleD_01 + base64Pen + moduleD_02; 
-		JSONObject d = new JSONObject(new JSONTokener(moduleD));
-		hal.insertModule(d, d);
-		
-		// workplane
-		String moduleE = moduleE_01 + base64WorkplaneRos + moduleE_02 + base64Pen + moduleE_03;
-		JSONObject e = new JSONObject(new JSONTokener(moduleE));
-		hal.insertModule(e, e);
 		
 		
-		JSONObject criteria = new JSONObject();
-		JSONObject target = new JSONObject();
-		JSONObject targetMove = new JSONObject();
-		targetMove.put("x", -2.0);
-		targetMove.put("y", 1.0);
-		targetMove.put("z", 15.0);
-		target.put("move",targetMove);
-		target.put("identifier", "GC4x4MB_1");
-		
-		JSONArray subjects = new JSONArray();
-		JSONObject subject = new JSONObject();
-		JSONObject subjectMove = new JSONObject();
-		subjectMove.put("x", -3.0);
-		subjectMove.put("y", 3.0);
-		subjectMove.put("z", 35.0);
-		subject.put("move",subjectMove);
-		subject.put("identifier", "GC4x4MB_6");
-		subjects.put(subject);
-		
-		criteria.put("target",target);
-		criteria.put("subjects", subjects);
-		
-		
-		hal.translateProductStep(new ProductStep(1, criteria, new Service("place")));
+		//hal.translateProductStep(new ProductStep(1, criteria, new Service("place")));
 		// Sending reloadEquiplet command to Blackboard
 		// UNTESTED W.I.P (Lars Veenendaal)]
+		hal.deleteModule(new ModuleIdentifier("HU", "dummy_module_type_A", "1"));
+		hal.deleteModule(new ModuleIdentifier("HU", "dummy_module_type_B", "1"));
 		hal.sendReloadEquiplet();
 		/*Service service = new Service("PickAndPlace");
 		ProductStep productStep = new ProductStep(0, null, service);
