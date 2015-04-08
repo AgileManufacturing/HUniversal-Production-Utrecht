22b653bf6a15314c81162d1dd9ef44b63dcb88dd
Pim te Slaa <pimteslaa@gmail.com>
2015-04-08 11:54:49 +0200

    Interface test implementation

--- src/REXOS/MAS/testerClasses/EquipletAgentTest.java
+++ src/REXOS/MAS/testerClasses/EquipletAgentTest.java
@@ -1,6 +1,37 @@
 package MAS.testerClasses;
 
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+import java.util.Date;
+
+import util.configuration.ServerConfigurations;
+import MAS.equiplet.EquipletAgent;
+
 
 public class EquipletAgentTest {
-	System.out.println("helloworld");
+	
+	private static final String MAIN_PORT = ServerConfigurations.GS_PORT;
+	private static final String CONTAINER_NAME = "EquipletAgent";
+	private static final String MAIN_HOST = ServerConfigurations.GS_IP;
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) throws StaleProxyException {
+		Date date = new Date();
+		// Spawning EquipletAgent in the container that has the selected IP/Port
+		jade.core.Runtime runtime = jade.core.Runtime.instance();
+		Profile profile = new ProfileImpl();
+		profile.setParameter(Profile.MAIN_HOST, MAIN_HOST);
+		profile.setParameter(Profile.MAIN_PORT, MAIN_PORT);
+		profile.setParameter(Profile.CONTAINER_NAME, CONTAINER_NAME);
+
+		AgentContainer container = runtime.createAgentContainer(profile);
+
+		Object[] arguments = new Object[] { "hal" };
+		AgentController ac = container.createNewAgent("EQ2", EquipletAgent.class.getName(), arguments);
+		ac.start();
+	}
 }
