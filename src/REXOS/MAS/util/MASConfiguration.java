0d7472eaadeee96718d8c34066e8437d78a8cb22
Laurens <laurensvdbrink@hotmail.com>
2015-04-20 13:31:31 +0200

    typo
    history typo

--- src/REXOS/MAS/util/MASConfiguration.java
+++ src/REXOS/MAS/util/MASConfiguration.java
@@ -5,12 +5,12 @@ public class MASConfiguration {
 	/**
 	 * VERBOSITY gives the level of simulation output
 	 * 0: none output is given while running the simulation
-	 * 1: important simulation debug is printed to the console
+	 * 1: important simulation debug information is printed to the console
 	 * 2: the gui is used for information during the simulation
 	 * 3: the gui is used in combination with the debug information in the console
 	 * 4: all above with addition of scheduling information
 	 */
-	public static int VERBOSITY = 0;
+	public static int VERBOSITY = 1;
 
 	/**
 	 * the number of jobs an just arrives job can overtake in the queue
@@ -20,7 +20,7 @@ public class MASConfiguration {
 	/**
 	 * Scheduling algorithm used by product agent to schedule his product step
 	 */
-	public final static SchedulingAlgorithm SCHEDULING = SchedulingAlgorithm.SUPREME;
+	public final static SchedulingAlgorithm SCHEDULING = SchedulingAlgorithm.MATRIX;
 
 	/**
 	 * Reshedule when start time of product step is not met
@@ -31,7 +31,7 @@ public class MASConfiguration {
 	 * keep track of the equiplet history while the equiplet agent is alive
 	 * this could lead to heavy memory use
 	 */
-	public final static boolean KEEP_FULL_EQUIPLET_HISORY = false;
+	public final static boolean KEEP_FULL_EQUIPLET_HISTORY = false;
 	
 	/**
 	 * name of the traffic controller agent
