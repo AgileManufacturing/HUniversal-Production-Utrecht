c48e53280fbc7836ccae834343d060ae1d021a3e
pimteslaa <pimteslaa@gmail.com>
2015-04-24 10:08:20 +0200

    Created SCADA package with WebSocketServer. Server still needs implementation.

---
+++ src/REXOS/SCADA/SCADAWebSocketServer.java
@@ -0,0 +1,36 @@
+package SCADA;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+public class SCADAWebSocketServer extends WebSocketServer {
+	
+	public SCADAWebSocketServer(int port) throws UnknownHostException {
+		super(new InetSocketAddress(port));
+	}
+	
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		
+	}
+	
+
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+
+	}
+	
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+
+	}
+}
