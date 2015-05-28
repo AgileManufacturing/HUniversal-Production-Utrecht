adc869d9a24ea02c2171b1d769c8c6affa5f1c58
pimteslaa <pimteslaa@gmail.com>
2015-05-28 11:11:03 +0200

    Deleted WebSocketListener interface and WebSocketContainer, because the architecture changed.

--- src/REXOS/SCADA/SCADAWebSocketServer.java
+++ src/REXOS/SCADA/SCADAWebSocketServer.java
@@ -15,33 +15,27 @@ import org.java_websocket.server.WebSocketServer;
 
 public class SCADAWebSocketServer extends WebSocketServer {
 	
-	private WebSocketListener theListener;
-	
 	public SCADAWebSocketServer(int port) throws UnknownHostException {
 		super(new InetSocketAddress(port));
 	}
 	
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
-		theListener.addConnection(conn);
+		
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, String message) {
-		theListener.sendMessage(conn, message);
+
 	}
 	
 	@Override
 	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-		theListener.closeConnection(conn);
+	
 	}
 
 	@Override
 	public void onError(WebSocket conn, Exception ex) {
 
-	}
-	
-	public void setListener(WebSocketListener l) {
-		theListener = l;
 	}
 }
