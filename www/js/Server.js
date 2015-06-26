d02f534571853f8cc67352bfab67ddd56e84b3e9
Benno Zeeman <bzeeman@live.nl>
2015-06-26 11:16:59 +0200

    Classes in new files, popup vendors, and new date format library

---
+++ www/js/Server.js
@@ -0,0 +1,198 @@
+"use strict";
+
+/**
+ * @Author Benno Zeeman
+ */
+function Server(host, port, id) {
+    this.id  = id;
+    this.url = host + ':' + port;
+
+    // When true will attempt reconnecting when disconnect
+    this.will_reconnect = true;
+    // Connection status
+    this.is_connected    = false;
+    // The WebSocket instance
+    this.websocket    = null;
+
+    // The UI object used to let the world know what the f*ck we're doing
+    this.ui = new UIPanel();
+    this.ui.click_agent_listener  = (this.requestAgent).bind(this);
+    this.ui.click_stop_listener   = (this.disconnect).bind(this);
+    this.ui.click_start_listener  = (this.connect).bind(this);
+    this.ui.create_agent_listener = (this.createAgent).bind(this);
+    this.ui.switch_listener       = (this.switch).bind(this);
+
+    this.ui.println('Server("' + host + '", ' + port + ')', this.id);
+
+    this.section = this.SECTION_GRID;
+}
+
+// Server methods
+Server.prototype = {
+    'onerror': function(event) {
+        if (!this.is_connected) {
+            return;
+        }
+        this.ui.println('WebSocket error', this.id);
+        //console.log('onerror (event next)');
+        //console.log(event);
+    },
+    'onmessage': function(event) {
+        var data = JSON.parse(event.data);
+
+        this.ui.println('Received "' + data['command'] + '"', this.id);
+
+        // Switch what type of message is received
+        // Act accordingly
+        switch (data['command']) {
+            case 'GET_OVERVIEW':
+                for(var i = 0; i < data['agents'].length; i++) {
+                    var agent = data['agents'][i];
+                    this.ui.addAgent(agent);
+                }
+
+                break;
+            case 'ADD_AGENT':
+                this.ui.addAgent(data['agent']);
+                break;
+            case 'UPDATE_AGENT':
+                this.ui.updateAgent(data['agent']);
+                break;
+            case 'GET_DETAILED_INFO':
+                this.ui.setAgent(data['agent']);
+
+                break;
+            default:
+                this.ui.println('Unknown "' + data['command'] + '"', this.id);
+                break;
+        }
+    },
+    'onopen': function(event) {
+        this.ui.println('Connected to web socket', this.id);
+
+        // Clear potential timeout trying to reconnect
+        clearTimeout(this.timeout);
+
+        this.ui.setConStatus(this.ui.OPEN);
+        this.is_connected = true;
+
+        // Request grid overview from WS
+        this.send('GET_OVERVIEW');
+    },
+    'onclose': function(event) {
+        // Find respective descriptions for error codes
+        // https://tools.ietf.org/html/rfc6455#section-11.7
+        switch (event.code) {
+            case 1000: var desc = 'Normal Closure';             break;
+            case 1001: var desc = 'Going Away';                 break;
+            case 1002: var desc = 'Protocol error';             break;
+            case 1003: var desc = 'Unsupported Data';           break;
+            case 1005: var desc = 'No Status Rcvd';             break;
+            case 1006: var desc = 'Abnormal Closure';           break;
+            case 1007: var desc = 'Invalid frame payload data'; break;
+            case 1008: var desc = 'Policy Violation';           break;
+            case 1009: var desc = 'Message Too Big';            break;
+            case 1010: var desc = 'Mandatory Ext.';             break;
+            case 1011: var desc = 'Internal Server Error';      break;
+            case 1015: var desc = 'TLS handshake';              break;
+            default:   var desc = 'Unrecognized error code';    break;
+        }
+
+        //console.log('onclose (event next)');
+        //console.log(event);
+
+        // If is_connected is false, connection was closed purposedly
+        if (this.is_connected) {
+            this.ui.println('Connection closed with "' + desc + '"', this.id);
+            this.is_connected = false;
+        }
+
+        this.ui.setConStatus(this.ui.CLOSED);
+        this.websocket    = null;
+
+        if (this.will_reconnect) {
+            this.timeout = setTimeout((this.connect).bind(this), 3000);
+        }
+    },
+
+    'connect': function() {
+        this.ui.println('Attempting to connect to "' + this.url + '"', this.id);
+        this.ui.setConStatus(this.ui.CONNECTING);
+
+        this.will_reconnect = true;
+
+        this.websocket = new WebSocket('ws://' + this.url);
+
+        // Callbacks with correct context (this)
+        this.websocket.onerror   = (this.onerror).bind(this);
+        this.websocket.onmessage = (this.onmessage).bind(this);
+        this.websocket.onclose   = (this.onclose).bind(this);
+        this.websocket.onopen    = (this.onopen).bind(this);
+
+        return this.websocket;
+    },
+
+    'disconnect': function() {
+        this.ui.println('Stopping client', this.id);
+        this.ui.setConStatus(this.ui.CLOSED);
+
+        this.ui.cleanGrid();
+
+        this.is_connected      = false;
+        this.will_reconnect = false;
+        if (this.timeout !== null) {
+            clearTimeout(this.timeout);
+        }
+
+        //if (this.websocket !== null) {
+            this.websocket.close();
+        //  this.websocket = null;
+        //}
+    },
+
+    // Send to WS
+    'send': function(command, agent) {
+        var json = {'command': command};
+        if (typeof(agent) !== 'undefined' ) {
+            json['agent'] = agent;
+        }
+
+        // Create JSON message to send over WS
+        var str = JSON.stringify(json);
+
+        this.websocket.send(str);
+    },
+
+    'toJson': function() {
+
+    },
+
+    'createAgent': function(agent) {
+        if (!this.is_connected) {
+            return;
+        }
+
+        this.ui.println('Create Agent "' + agent['id'] + '"', this.id);
+
+        this.send('CREATE_AGENT', agent);
+    },
+    'requestAgent': function(id) {
+        if (!this.is_connected) {
+            return;
+        }
+
+        this.ui.println('Get Agent "' + id + '" information from SCADA', this.id);
+
+        var agent = {'id': id};
+        this.send('GET_AGENT_INFO', agent);
+    },
+
+    'switch': function(agent) {
+        // Re-request overview, and clean grid when leaving section
+        if (agent === 'grid') {
+            this.send('GET_OVERVIEW');
+        } else if (agent === 'agent') {
+            this.ui.cleanGrid();
+        }
+    },
+};
---
+++ www/js/UIPanel.js
@@ -0,0 +1,321 @@
+"use strict";
+
+/**
+ * @Author Benno Zeeman
+ */
+function UIPanel() {
+    // Navigation
+    $('#nav ul > li > a').click((this.navClick).bind(this));
+    this.switch_listener = null;
+
+
+
+    // Log section
+    this.log_pre   = $('#log pre');
+    this.log_clean = $('#log #clear');
+    this.log_clean.click((this.clearLog).bind(this));
+
+
+
+    // Grid section
+    this.grid_list = $('#grid ul');
+    this.agents     = {};
+
+    // MagnificPopup opens form on pressing button
+    this.btn_create_agent  = $('#grid #create-agent-btn');
+    this.form_create_agent = $('#grid #create-agent-form');
+
+    this.form_create_agent.submit((this.submitCreateAgent).bind(this));
+    this.btn_create_agent.magnificPopup({'items': { 'src':  '#create-agent-form', 'type': 'inline' }});
+
+    this.create_agent_listener = null;
+
+    this.click_agent_listener = null;
+
+
+
+    // Agent section
+    this.agent_form = $('#agent > form');
+    this.agent      = {};
+
+
+
+    // Controls section
+    this.con_status_light = $('#controls #con-status .light');
+    this.con_status_text  = $('#controls #con-status .text');
+
+    this.btn_stop  = $('#controls #stop').click((this.clickStop).bind(this));
+    this.btn_start = $('#controls #start').click((this.clickStart).bind(this));
+
+    this.click_stop_listener  = null;
+    this.click_start_listener = null;
+
+    this.setConStatus(this.CLOSED);
+}
+
+UIPanel.prototype = {
+    'CLOSED':      0,
+    'CONNECTING':  1,
+    'OPEN':        2,
+    'println': function(msg, id) {
+        // If argument(s) not defined, give default
+        id = id || '';
+
+        // Moment.js gives us the formatting!
+        var time_str = moment().format('HH:mm:ss.SSS');
+
+        // If scrolled to bottom, remember and scroll down after new content
+        var bottom = this.log_pre.prop('scrollHeight') - this.log_pre.scrollTop() == this.log_pre.innerHeight();
+
+        // Create container with elements
+        var $container =
+            $('<div></div>', {'class': 'line'})
+            .append($('<em></em>',     {'class': 'time'}).text(time_str))
+            .append($('<em></em>',     {'class': 'id'}).text(id))
+            .append($('<span></span>', {'class': 'msg'}).text(msg))
+        ;
+
+        // Write the log message in certain format
+        this.log_pre.append($container);
+
+        // Scroll down for 'sticky' effect.
+        if (bottom === true) {
+            this.log_pre.scrollTop(this.log_pre.prop('scrollHeight'));
+        }
+    },
+    'clearLog': function(event) {
+        if (typeof(event) !== 'undefined') {
+            event.preventDefault();
+        }
+
+        this.log_pre.empty();
+    },
+
+
+    'addAgent': function(agent) {
+        if (!agent['id']) {
+            this.println('No ID to work with', 'ui');
+            return;
+        }
+        var id = agent['id'];
+
+        this.println('Adding "' + id + '"', 'ui');
+
+        // Create UI elements with attributes
+        var $li = $('<li></li>', {'data-id': id, 'data-type': agent['type']});
+        var $a  = $('<a></a>', {'href': '#', on: {click: (this.clickAgent).bind(this)}});
+
+        $li.append($a);
+
+        this.agents[id] = {};
+
+        for (var p in agent) {
+            var $div = $('<div></div>');
+            $div.attr('class', p);
+            $div.text(agent[p]);
+
+            $a.append($div);
+
+            this.agents[id][p] = $div;
+        }
+
+        this.grid_list.append($li);
+    },
+
+    'updateAgent': function(agent) {
+        if (!agent['id']) {
+            this.println('No ID to work with', 'ui');
+            return;
+        }
+        var id = agent['id'];
+
+        // Check if
+        if (this.agent['id'] === id) {
+            for (var p in agent) {
+                if (p === 'id') {
+                    continue;
+                }
+
+
+                // CHeck if form element exists that we want to update.
+                if (p in this.agent) {
+                    var value = agent[p];
+
+                    this.agent[p].val(value);
+                } else {
+                    this.println('Could not update value "' + p + '" Agent "' + id + '"', 'ui');
+                }
+            }
+        } else if (id in this.agents) {
+            this.println('Updating Agent in Grid', 'ui');
+
+            // Loop through values in agent, so we can update them (usually just 1)
+            for (var p in agent) {
+                // ID is the ID that we used to find the agent, so updating it makes no sense.
+                if (p === 'id') {
+                    continue;
+                }
+
+
+                // Check if value that we want to update is there
+                if (p in this.agents[id]) {
+                    // Get the value of the property we want to update
+                    var value = agent[p];
+
+                    this.agents[id][p].text(value);
+                } else {
+                    this.println('Could not update value "' + p + '" Agent "' + id + '" in grid section', 'ui');
+                }
+            }
+        } else {
+            this.println('Agent not found in grid nor agent section', 'ui');
+        }
+    },
+
+    'setAgent': function(agent) {
+        if (!agent['id']) {
+            this.println('No ID to work with', 'ui');
+            return;
+        }
+        var id = agent['id'];
+
+        // Clear form
+        this.agent_form.text('');
+        this.agent_form.data('id', id);
+
+        // Create 'div > input|select' for all properties
+        for (var name in agent) {
+            var $div     = $('<div></div>');
+            var $label   = $('<label></label>');
+            var $element = null;
+
+            switch (agent[name]['type']) {
+                case 'string':
+                    $label.text(name);
+
+                    $element = $('<input />')
+                        .attr('name', name)
+                        .val(agent[name]['value'])
+                        .attr('type', 'text')
+                        .attr('disabled', agent[name]['read-only'])
+                        .attr('required', agent[name]['required'])
+                    ;
+                    break;
+                default:
+                    this.println('Unknown type "' + agent[name]['type'] + '"', 'ui');
+                    break;
+            }
+
+            if ($element !== null) {
+                $div.append($label);
+                $div.append($element);
+
+                this.agent_form.append($div);
+
+                this.agent[name] = $element;
+            }
+        }
+
+        var $div    = $('<div></div>');
+        var $button = $('<button></button>');
+        $button.attr('type', 'submit');
+        $button.text('Submit');
+
+        $div.append($button);
+
+        this.agent_form.append($div);
+        this.switch('agent');
+    },
+
+
+    'setConStatus': function(con_status) {
+        if (con_status == this.CLOSED) {
+            this.con_status_light.css('background-color', 'red');
+            this.con_status_text.text('Closed');
+
+            this.btn_create_agent.prop('disabled', true);
+        } else if (con_status == this.CONNECTING) {
+            this.con_status_light.css('background-color', 'yellow');
+            this.con_status_text.text('Connecting');
+        } else if (con_status == this.OPEN) {
+            this.con_status_light.css('background-color', 'green');
+            this.con_status_text.text('Open');
+
+            this.btn_create_agent.prop('disabled', false);
+        }
+    },
+
+    'clickAgent': function(event) {
+        event.preventDefault();
+
+        // Retreive elements
+        var $a  = $(event.currentTarget);
+        var $li = $a.parent('li');
+        var id  = $li.data('id');
+
+        this.println('Clicked agent "' + id + '"', 'ui');
+
+        if (this.click_agent_listener !== null) {
+            this.click_agent_listener(id);
+        }
+    },
+    'clickStop': function(event) {
+        this.btn_stop.prop('disabled', true);
+        this.btn_start.prop('disabled', false);
+
+        if (this.click_stop_listener !== null) {
+            this.click_stop_listener();
+        }
+    },
+    'clickStart': function(event) {
+        this.btn_stop.prop('disabled', false);
+        this.btn_start.prop('disabled', true);
+
+        if (this.click_start_listener !== null) {
+            this.click_start_listener();
+        }
+    },
+
+    'submitCreateAgent': function(event) {
+        event.preventDefault();
+
+        $.magnificPopup.close();
+
+        console.log(this.create_agent_listener);
+
+        if (this.create_agent_listener !== null) {
+            this.create_agent_listener($('#create-agent-form').serializeObject());
+        }
+    },
+
+    'navClick': function(event) {
+        event.preventDefault();
+
+        var section = $(event.target).data('section');
+
+        this.switch(section);
+    },
+    'switch': function(section) {
+        $('main > section').hide();
+        $('#nav ul > li > a').removeClass('current');
+
+        $('#nav ul > li > a[data-section=' + section + ']').addClass('current');
+        $('main > section#' + section).show();
+
+        if (this.switch_listener !== null) {
+            this.switch_listener(section);
+        }
+    },
+
+    'cleanGrid': function() {
+        this.grid_list.html('');
+
+        this.agents = {};
+    },
+    'cleanAgent': function() {
+        this.agent_form.html();
+        this.agent_form.removeAttr('id');
+
+        this.agent = {};
+    },
+};
