dd83cbcf0cb4a9de2a9422a3f1ec1fa1dfcb54c7
Aesthy <edjekoek@hotmail.com>
2016-01-07 13:10:07 +0100

    Oops2

--- src/REXOS/ROS/huniversal_camera/vision_node/src/VisionNode.cpp
+++ src/REXOS/ROS/huniversal_camera/vision_node/src/VisionNode.cpp
@@ -175,7 +175,7 @@ void VisionNode::handleFrame(cv::Mat& camFrame) {
 		camFrame = fishEyeCorrector.handleFrame(camFrame);
 	}
         if(isStlNodeEnabled){
-            stlNode.handleFrame(camFrame.clone(),&camFrame);
+            stlNode.handleFrame(camFrame,&camFrame);
         }
 	cv::Mat grayScaleFrame;
 	if(isQrCodeReaderEnabled == true || isFudicialDetectorEnabled == true){
