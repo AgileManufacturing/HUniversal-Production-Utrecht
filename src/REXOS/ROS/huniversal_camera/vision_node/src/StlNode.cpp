3810be8855545cd1b17141964173dce7b96f9d9e
Aesthy <edjekoek@hotmail.com>
2016-01-18 12:28:55 +0100

    More includes!

--- src/REXOS/ROS/huniversal_camera/vision_node/src/StlNode.cpp
+++ src/REXOS/ROS/huniversal_camera/vision_node/src/StlNode.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 
-//#include <opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
 //#include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 //#include <opencv2/calib3d/calib3d.hpp>
 //#include <opencv2/objdetect/objdetect.hpp>
 
