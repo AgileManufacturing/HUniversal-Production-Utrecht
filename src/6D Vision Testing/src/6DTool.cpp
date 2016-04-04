e83575c58ad35123271f250b870ccbde2d0374c3
KjeldPerquin <kjeld.perquin@student.hu.nl>
2016-04-04 10:31:34 +0200

    Added optimalisation to build

--- src/6D Vision Testing/src/6DTool.cpp
+++ src/6D Vision Testing/src/6DTool.cpp
@@ -3,43 +3,6 @@
 #include <ObjectDetector.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
-cv::Point3d getOrientation(std::vector<cv::Point> &pts, cv::Mat img)
-{
-	//Construct a buffer used by the pca analysis
-	cv::Mat data_pts = cv::Mat(pts.size(), 2, CV_64FC1);
-	for (int i = 0; i < data_pts.rows; ++i)
-	{
-		data_pts.at<double>(i, 0) = pts[i].x;
-		data_pts.at<double>(i, 1) = pts[i].y;
-	}
-
-	//Perform PCA analysis
-	cv::PCA pca_analysis(data_pts, cv::Mat(), CV_PCA_DATA_AS_ROW);
-
-	//Store the position of the object
-	cv::Point pos = cv::Point(pca_analysis.mean.at<double>(0, 0),
-		pca_analysis.mean.at<double>(0, 1));
-
-	//Store the eigenvalues and eigenvectors
-	std::vector<cv::Point2d> eigen_vecs(2);
-	std::vector<double> eigen_val(2);
-	for (int i = 0; i < 2; ++i)
-	{
-		eigen_vecs[i] = cv::Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
-			pca_analysis.eigenvectors.at<double>(i, 1));
-
-		eigen_val[i] = pca_analysis.eigenvalues.at<double>(0, i);
-	}
-
-	// Draw the principal components
-	circle(img, pos, 3, cv::Scalar(128, 128, 128), 2);
-	//line(img, pos, pos + 0.02 * cv::Point(eigen_vecs[0].x * eigen_val[0], eigen_vecs[0].y * eigen_val[0]), cv::Scalar(128, 128, 128));
-	//line(img, pos, pos + 0.02 * cv::Point(eigen_vecs[1].x * eigen_val[1], eigen_vecs[1].y * eigen_val[1]), cv::Scalar(128, 128, 128));
-
-	return cv::Point3d(pos.x, pos.y, atan2(eigen_vecs[0].y, eigen_vecs[0].x));
-	//return atan2(eigen_vecs[0].y, eigen_vecs[0].x);
-}
-
 void checkParameters(int argc, char** argv)
 {
 	//The program expects two parameters from the user. Since C++ adds it's own first
