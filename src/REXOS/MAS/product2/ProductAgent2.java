9326993deef4569b6e7b58847e57d14d53ba8fd3
Wibren Wiersma <w.wiersma@emendis.nl>
2016-04-07 11:02:50 +0200

    Product agent2

---
+++ src/REXOS/MAS/product2/ProductAgent2.java
@@ -0,0 +1,42 @@
+package MAS.product;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import org.json.JSONException;
+
+import MAS.util.Tick;
+
+public class ProductAgent2 extends Agent {
+	private static final long serialVersionUID = -7419809733484399023L;
+	private Tick created;
+
+	public void setup() {
+		Object[] args = getArguments();
+		this.created = new Tick();
+	}
+
+	/**
+	 * @return time of agent creation
+	 */
+	protected Tick getCreated() {
+		return created;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductAgent2";
+	}
+}
