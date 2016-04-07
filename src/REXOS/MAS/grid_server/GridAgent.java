f5350e6ee129345030d51186585d9d5e72ba7eff
Wibren Wiersma <w.wiersma@emendis.nl>
2016-04-07 11:23:01 +0200

    Error fix

--- src/REXOS/MAS/grid_server/GridAgent.java
+++ src/REXOS/MAS/grid_server/GridAgent.java
@@ -273,7 +273,7 @@ public class GridAgent extends Agent {
     public void spawnProductAgent2(String name, String inputArguments){
         try {
 			ContainerController cc = getContainerController();
-            AgentController ac = cc.createNewAgent(name, ProductAgent.class.getName(), inputArguments);
+            AgentController ac = cc.createNewAgent(name, ProductAgent.class.getName(), null);
             ac.start();
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
